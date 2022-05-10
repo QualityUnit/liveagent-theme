9eda000cfdc58c17442ad20480e4993262fb64d9
Jozef Remeň <jozef.remen@gmail.com>
2022-05-10 14:39:38 +0200

    Schema.org additional fields

--- lib/widgets/qu-checklists/checklists.php
+++ lib/widgets/qu-checklists/checklists.php
@@ -11,12 +11,16 @@
  * Text Domain:       checklists
 */
 
-
 function qu_checklists_init() {
 	$domain = 'qu-checklists/';
 	$path   = get_parent_theme_file_path( '/lib/widgets/' . $domain );
 
 	require_once $path . 'layouts/checklist-item.php';
+
+	function get_post_id() {
+		global $post;
+		return $post->ID;
+	}
 
 	// Resources for editor/admin part
 	function checklists_editor_assets() {
@@ -143,8 +147,18 @@ function qu_checklists_init() {
 		} else {
 			$pagetitle = get_the_title();
 		}
+
+		$post_content = apply_filters( 'the_content', $content );
+
+		// Calculating reading time of the post
+		$read_time = ceil( strlen( wp_strip_all_tags( $post_content ) ) * 0.035 / 60 ); // About 0,035 seconds per char;
+
 		return '<div class="qu-Checklist Post__m__negative" itemscope itemtype="https://schema.org/HowTo"
 							data-list="' . $attr['checklistId'] . '">
+							<meta itemprop="supply" content="' . $attr['supply'] . '">
+							<meta itemprop="tool" content="' . $attr['tool'] . '">
+							<meta itemprop="totalTime" content="PT' . strlen( $attr['totalTime'] ) > 0 ? $attr['totalTime'] : $read_time . 'M">
+							
 							<span itemprop="name" style="display:none">' . $pagetitle . '</span>' .
 								apply_filters( 'the_content', $content )
 						. '</div>';
@@ -161,6 +175,18 @@ function qu_checklists_init() {
 			'attributes'                 => array(
 				'checklistId' => array(
 					'type' => 'string',
+				),
+				'supply'      => array(
+					'type'    => 'string',
+					'default' => 'Help Desk Software',
+				),
+				'tool'        => array(
+					'type'    => 'string',
+					'default' => 'LiveAgent',
+				),
+				'totalTime'   => array(
+					'type'    => 'string',
+					'default' => '',
 				),
 			),
 		),
