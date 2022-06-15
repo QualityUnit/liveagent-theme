d1227626c458f5e7fed343c043d1fe36cb6c8bd1
Jozef Remen <jozef.remen@gmail.com>
2022-06-15 08:25:40 +0200

    [503] fix: allowing click on whole play button in vertical video box (#190)
    * fix: allowing click on whole play button in vertical video box
    
    * fix: allowing pinch to zoom on androids

--- templates/head.php
+++ templates/head.php
@@ -1,7 +1,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://cdn.liveagent.com">
