6082c7a88d6c8b9d9f03df3e6fe14526e6b21983
Rastislav Laco <rlaco@qualityunit.com>
2022-06-22 17:32:35 +0200

    feat: preparing elementor structure for sidebars

--- functions/content-filters.php
+++ functions/content-filters.php
@@ -365,7 +365,7 @@ function replace_admin_subdomain( $content ) {
 add_filter( 'the_content', 'replace_admin_subdomain' );
 
 
-/* 
+/*
 * Change formatting in Block--learnMore <pre> blocks
 */
 
@@ -413,3 +413,108 @@ function learnmore_pre_block( $content ) {
 	return $content;
 }
 add_filter( 'the_content', 'learnmore_pre_block', 9999 );
+
+
+function learnmore_sidebars( $content ) {
+	if ( is_page_template( 'elementor.php' ) ) {
+		if ( ! $content ) {
+			return $content;
+		}
+
+		$dom = new DOMDocument();
+		libxml_use_internal_errors( true );
+		$dom->loadHTML( mb_convert_encoding( $content, 'HTML-ENTITIES', 'UTF-8' ) );
+		libxml_clear_errors();
+		$xpath       = new DOMXPath( $dom );
+		$block_class = 'xxxx';//Block--learnMore
+		$blocks      = get_nodes( $xpath, $block_class );
+
+		// @codingStandardsIgnoreStart
+		//$dom->appendChild($dom->createTextNode( 'dlzka blokov:'.count($blocks) ));
+		if ( count($blocks) > 0 ) {
+
+			$clText = 'elementor-text-editor elementor-clearfix';
+			$clContent = 'Content';
+			foreach ( $blocks as $block ) {
+				//remove Content class from section
+				$blockClasses = $block->getAttribute( 'class' );
+				$blockClasses = str_replace( ' ' . $clContent, '', $blockClasses);
+				$block->setAttribute( 'class', $blockClasses );
+				$divIndex = 0;
+
+				foreach ( $block->getElementsByTagName( 'div' ) as $div ) {
+					if ( $div->getAttribute( 'class' ) == $clText ) {
+						if( $divIndex == 1 ) {
+
+							$wrapEl = $dom->createElement('div');
+							$wrapEl->setAttribute('class',$clText . ' ' . $clContent . ' wrapper__wide Post__container');
+							$wrap = $wrapEl->cloneNode();
+							$div->parentNode->replaceChild($wrap,$div);
+							$wrap->appendChild($div);
+
+							$wrap->firstChild->setAttribute('class','BlogPost__content Post__content');
+
+							$wrap->insertBefore($dom->createTextNode( 'sidebar 1' ),$wrap->firstChild);
+							$wrap->appendChild($dom->createTextNode( 'sidebar 2' ));
+
+							/*$childsCount = $div->childNodes->length;
+							$div->insertBefore($dom->createTextNode( $childsCount ),$div->firstChild);*/
+
+							/*$childrens = $div->cloneNode(true)->childNodes;
+							$new_div_clone = $new_div->cloneNode();
+							$div->insertBefore($new_div_clone,$div->firstChild);
+							foreach ( $childrens as $child ) {
+								$childClone = $child->cloneNode( true );
+								$div->firstChild->appendChild($childClone);
+							}*/
+
+							/*for($i=3;$i<50;$i++){
+								$div->removeChild($div->childNodes->item($i));
+							}*/
+
+							/*$i = 0;
+							$newChildrens  = $div->childNodes;
+							foreach ( $newChildrens as $child ) {
+								if ( $i != 0 ) {
+									$div->removeChild($child);
+								}
+								$i++;
+							}*/
+
+							/*while ($div->hasChildNodes()) {
+								$div->removeChild($div->firstChild);
+							}*/
+
+
+
+						} else {
+							$div->setAttribute( 'class', $clText . ' ' . $clContent );
+						}
+						$divIndex++;
+					}
+				}
+			}
+
+		} else {
+
+			$sidebarLeftClass	= 'Post__sidebar';
+			$sidebarLeft		= get_nodes( $xpath, $sidebarLeftClass );
+
+			$dom->appendChild($dom->createTextNode( 'dlzka blokov:'.count($blocks) ));
+
+			foreach ( $sidebarLeft as $sidebar ){
+				$sidebar->appendChild($dom->createTextNode( 'xxxxx' ));
+			}
+
+		}
+
+		// @codingStandardsIgnoreEnd
+
+		$dom->removeChild( $dom->doctype );
+		$content = $dom->saveHtml();
+		$content = str_replace( '<html><body>', '', $content );
+		$content = str_replace( '</body></html>', '', $content );
+		return '[signup-sidebar]' . $content;
+	}
+}
+add_filter( 'the_content', 'learnmore_sidebars', 9999 );
