e2478daa77ac365e2e3838cfc79293343a18c804
Jozef Remen <jozef.remen@gmail.com>
2022-07-11 09:03:07 +0200

    [481] fix: automatic image switcher on block--switcher (#280)
    * fix: automatic image switcher on block--switcher
    
    * fix: adding switcher button switching on image change

--- assets/scripts/app/toggleVisibility.js
+++ assets/scripts/app/toggleVisibility.js
@@ -1,6 +1,8 @@
 const activators = document.querySelectorAll( '[data-target]' );
 const targets = document.querySelectorAll( `[data-targetId]` );
 
+let isPaused = false;
+
 if ( activators.length > 0 ) {
 	activators.forEach( ( elem ) => {
 		const activator = elem;
@@ -20,6 +22,7 @@ if ( activators.length > 0 ) {
 			);
 
 			hideVisible();
+			isPaused = true;
 
 			thisTarget.classList.remove( 'hidden' );
 			thisActivator.classList.add( 'active' );
@@ -29,3 +32,28 @@ if ( activators.length > 0 ) {
 		} );
 	} );
 }
+
+const switcher = document.querySelector( '.Block--switcher' );
+( function autoSwitch( interval = 5000 ) {
+	if ( ! isPaused ) {
+		const notCheckedInput = switcher.querySelector( '.switcher__input:not(:checked)' );
+		const isActive = switcher.querySelector( '.visible[data-targetid]' );
+		const isHidden = switcher.querySelector( '.hidden[data-targetid]' );
+
+		notCheckedInput.checked = true;
+
+		isActive.classList.remove( 'visible' );
+		isActive.classList.add( 'hidden' );
+
+		isHidden.classList.remove( 'hidden' );
+
+		setTimeout( () => {
+			isHidden.classList.add( 'visible' );
+		}, 200 );
+
+		setTimeout( () => {
+			autoSwitch();
+		}, interval );
+	}
+}() );
+
