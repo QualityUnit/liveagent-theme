aba08d842b9989ac9a6b4d83bbd59a19ffc683f1
Matúš Búci <matusbuci@gmail.com>
2022-07-19 13:47:33 +0200

    [494] feat: pricing shortcode redesign (#250)
    * feat: pricing shortcode redesign
    
    * feat: added tags
    
    * style: adjusted responsivity, added background and support for tags
    
    * fix: added default values, texts are now translatable
    
    * fix: added translations for trial buttons
    
    * style: shortcode has full width
    
    * style: removed stars background
    
    * style: setting color without using @include
    
    * fix: month in price is set with php instead of css
    
    * style: hidden button and best price
    
    * fix: merging code for both designes into one
    
    * feat: added button to shortcode
    
    * style: fixed margins and sizes
    
    * fix: fixed padding and background on elements that uses Block--background
    
    * feat: buttons are now translatable

--- lib/shortcodes/block-pricing.php
+++ lib/shortcodes/block-pricing.php
@@ -3,16 +3,14 @@
 function ms_block_pricing( $atts ) {
 	$atts = shortcode_atts(
 		array(
-			'image-link'        => '',
-			'title'             => '',
-			'text'              => '',
-			'price'             => '',
-			'buttonText'        => 'Try it',
-			'buttonLink'        => '/trial/',
-			'second_image-link' => '',
-			'second_title'      => '',
-			'second_text'       => '',
-			'second_price'      => '',
+			'title1' => 'Ticket',
+			'price1' => '15$',
+			'title2' => 'Ticket+chat',
+			'price2' => '29$',
+			'title3' => 'All-Inclusive',
+			'price3' => '49$',
+			'title4' => 'Free',
+			'price4' => '0$',
 		),
 		$atts,
 		'block_pricing'
@@ -22,52 +20,142 @@ function ms_block_pricing( $atts ) {
 	?>
 
 	<div class="BlockPricing__container">
-
-		<div class="BlockPricing__container__item PricingTable">
-			<div class="PricingTable__header green">
-				<div class="PricingTable__header__icon">
-					<img src="<?= esc_url( $atts['image-link'] ); ?>" alt="<?= esc_attr( $atts['title'] ); ?>">
+		<div class="BlockPricing__container__item">
+			<div class="PricingTable__header">
+				<div class="PricingTable__header__price">
+					<span class="PricingTable__header__price__amount"><?= esc_html( $atts['price1'] ); ?></span>
+					<span class="PricingTable__header__price__month"><?php _e( 'month', 'ms' ); ?></span>
 				</div>
 				<div class="PricingTable__header__title">
-					<h3><?= esc_html( $atts['title'] ); ?></h3>
+					<h3><?= esc_html( $atts['title1'] ); ?></h3>
 				</div>
 				<div class="PricingTable__header__description">
-					<p><?= esc_html( $atts['text'] ); ?></p>
-				</div>
-				<div class="PricingTable__header__price">
-					<span class="PricingTable__header__price__amount"><?= esc_html( $atts['price'] ); ?></span>
+					<ul>
+						<li class="checkmark">
+							<span><?php _e( 'Unlimited ticket history', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark" >
+							<span><?php _e( 'Unlimited email addresses', 'ms' ); ?></span>
+						</li>
+						<li  class="checkmark">
+							<span><?php _e( 'Advanced reporting', 'ms' ); ?></span>
+						</li>
+						<li  class="checkmark">
+							<span><?php _e( 'Customer portal + forum', 'ms' ); ?></span>
+						</li>
+					</ul>
 				</div>
 				<div class="PricingTable__header__button">
-					<a href="<?= esc_url( $atts['buttonLink'] ); ?>" class="Button Button--outline">
-						<span><?= esc_html( $atts['buttonText'] ); ?></span>
+					<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--outline">
+						<span><?php _e( 'Try for FREE', 'ms' ); ?></span>
+					</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="BlockPricing__container__item">
+			<div class="PricingTable__header">
+				<div class="PricingTable__header__price">
+					<span class="PricingTable__header__price__amount"><?= esc_html( $atts['price2'] ); ?></span>
+					<span class="PricingTable__header__price__month"><?php _e( 'month', 'ms' ); ?></span>
+				</div>
+				<div class="PricingTable__header__title">
+					<h3><?= esc_html( $atts['title2'] ); ?></h3>
+				</div>
+				<div class="PricingTable__header__description">
+					<ul">
+						<li class="italic">
+							<span><?php _e( 'Everything in Ticket, plus:', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( 'Unlimited chat buttons', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( 'Feedback management', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( 'Real time visitors monitor', 'ms' ); ?></span>
+						</li>
+					</ul>
+				</div>
+				<div class="PricingTable__header__button">
+					<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--outline">
+						<span><?php _e( 'Try for FREE', 'ms' ); ?></span>
 					</a>
 				</div>
 			</div>
 		</div>
-
-		<div class="BlockPricing__container__item PricingTable">
-			<div class="PricingTable__header green">
-				<div class="PricingTable__header__icon">
-					<img src="<?= esc_url( $atts['second_image-link'] ); ?>" alt="<?= esc_attr( $atts['title'] ); ?>">
+		<div class="BlockPricing__container__item">
+			<div class="PricingTable__header popular">
+				<div class="PricingTable__header__price">
+					<span class="PricingTable__header__price__amount"><?= esc_html( $atts['price3'] ); ?></span>
+					<span class="PricingTable__header__price__month"><?php _e( 'month', 'ms' ); ?></span>
 				</div>
 				<div class="PricingTable__header__title">
-					<h3><?= esc_html( $atts['second_title'] ); ?></h3>
+					<h3><?= esc_html( $atts['title3'] ); ?></h3>
 				</div>
 				<div class="PricingTable__header__description">
-					<p><?= esc_html( $atts['second_text'] ); ?></p>
-				</div>
-				<div class="PricingTable__header__price">
-					<span class="PricingTable__header__price__amount"><?= esc_html( $atts['second_price'] ); ?></span>
+					<ul>
+						<li class="italic">
+							<span><?php _e( 'Everything in Ticket + chat, plus:', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( 'Call center support', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( 'Video call', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( 'IVR', 'ms' ); ?></span>
+						</li>
+					</ul>
 				</div>
 				<div class="PricingTable__header__button">
-					<a href="<?= esc_url( $atts['buttonLink'] ); ?>" class="Button Button--outline">
-						<span><?= esc_html( $atts['buttonText'] ); ?></span>
+					<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--outline">
+						<span><?php _e( 'Try for FREE', 'ms' ); ?></span>
+					</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="BlockPricing__container__item">
+			<div class="PricingTable__header">
+				<div class="PricingTable__header__price">
+					<span class="PricingTable__header__price__amount"><?= esc_html( $atts['price4'] ); ?></span>
+					<span class="PricingTable__header__price__month"><?php _e( 'month', 'ms' ); ?></span>
+				</div>
+				<div class="PricingTable__header__title">
+					<h3><?= esc_html( $atts['title4'] ); ?></h3>
+				</div>
+				<div class="PricingTable__header__description">
+					<ul>
+						<li class="checkmark">
+							<span><?php _e( '7 days ticket history', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( '1 chat button', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( '1 phone number', 'ms' ); ?></span>
+						</li>
+						<li class="checkmark">
+							<span><?php _e( '1 email address', 'ms' ); ?></span>
+						</li>
+					</ul>
+				</div>
+				<div class="PricingTable__header__button">
+					<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--outline">
+						<span><?php _e( 'Create', 'ms' ); ?></span>
 					</a>
 				</div>
 			</div>
 		</div>
-
-
+	</div>
+	<div class="BlockPricing__button">
+		<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--full">
+			<span><?php _e( 'Compare plans', 'ms' ); ?></span>
+			<span class="ButtonPricing--arrow">&#8594;</span>
+		</a>
 	</div>
 
 	<?php
