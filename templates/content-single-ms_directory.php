2f13082984a9a410ec1e39a849004139cf3c1463
Jozef Remen <jozef.remen@gmail.com>
2022-07-21 14:24:36 +0200

    [595] fix: missing url and safari hover (#330)
    * fix: missing url and safari hover
    
    * fix: small polishing
    
    Co-authored-by: Martin Stepanek <martin@stepanek.sk>

--- templates/content-single-ms_directory.php
+++ templates/content-single-ms_directory.php
@@ -166,9 +166,9 @@
 				<?php
 				if ( preg_match( '/\<img/', $screenshot ) ) {
 					?>
-				<a class="Directory__screenshot" href="<?= esc_url( get_post_meta( get_the_ID(), 'company_url', true ) ); ?>" target="_blank" title="<?= esc_attr( __( 'Go to', 'ms' ) . ' ' . get_post_meta( get_the_ID(), 'company_url', true ) . ' ' . __( 'Homepage', 'ms' ) ); ?>">
+				<a class="Directory__screenshot" href="<?= esc_url( get_post_meta( get_the_ID(), 'mb_directory_mb_directory_website', true ) ); ?>" target="_blank" title="<?= esc_attr( __( 'Go to', 'ms' ) . ' ' . get_post_meta( get_the_ID(), 'mb_directory_mb_directory_website', true ) ); ?>">
 					<div class="Directory__screenshot--url">
-					<?= esc_html( __( 'Go to', 'ms' ) . ' ' . get_post_meta( get_the_ID(), 'company_url', true ) ); ?>
+					<?= esc_html( __( 'Go to', 'ms' ) . ' ' . get_post_meta( get_the_ID(), 'mb_directory_mb_directory_website', true ) ); ?>
 					</div>
 					<img src="<?= esc_url( get_template_directory_uri() . '/assets/images/browser_window.svg' ); ?>" />
 				<?= $screenshot; // @codingStandardsIgnoreLine ?>
