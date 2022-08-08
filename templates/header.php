57d17267a9f7eb88c43d4909fd6fa001a3034a9c
Simon-w66 <patrixstar565@gmail.com>
2022-08-08 16:46:56 +0200

    feat(web): add demo button

--- templates/header.php
+++ templates/header.php
@@ -34,6 +34,9 @@
 				?>
 
 				<div class="Header__navigation__buttons">
+					<a href="<?php _e( '/demo/', 'ms' ); ?>" class="Button Button--outline" onclick="_paq.push(['trackEvent', 'Activity', 'Header', 'Demo'])">
+						<span><?php _e( 'Demo', 'ms' ); ?></span>
+					</a>
 					<a href="<?php _e( '/trial/', 'ms' ); ?>" class="Button Button--full" onclick="_paq.push(['trackEvent', 'Activity', 'Header', 'Free Trial'])">
 						<span><?php _e( 'Free Trial', 'ms' ); ?></span>
 					</a>
