6ab28879d57b4df7e8a8985692782933c8fbd0ec
Jozef Remeň <jozef.remen@gmail.com>
2022-08-22 08:49:56 +0200

    fix: removing lazyloading from custom functions (moved to urlslab plugin)

--- functions.php
+++ functions.php
@@ -31,8 +31,6 @@ define( 'THEME_VERSION', '1.0.18' );
 		'functions/sideribbon-arrow.php', // Adds SVG with bookmark like ending
 		'functions/import-functions.php', // Partials JS and SCSS import functions
 		'functions/sidebar-toc.php', // TOC sidebar in features, integrations, blogs etc.
-		'functions/lazy-load.php', // Lazy loading of images, videos etc.
-		'functions/lazy-load-youtube-microdata.php', // Lazy loading and Microdata for YouTube videos.
 		'functions/post-types.php', // Import Custom Post Types
 		'functions/taxonomies.php', // Import Custom Taxonomies
 		'functions/metaboxes.php', // Import Metaboxes
