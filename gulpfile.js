4f4c4f8bddccad050d8b0eb45fbd7651b43b75a3
Jozef Remeň <jozef.remen@gmail.com>
2022-10-19 13:45:15 +0200

    fix: changing menu icons for svg icons

--- gulpfile.js
+++ gulpfile.js
@@ -129,7 +129,22 @@ gulp.task( 'iconsSprite', () =>
 		.src( [
 			'./vendor/qualityunit/wordpress-icons/icons/common/**/*.svg',
 			'./vendor/qualityunit/wordpress-icons/icons/liveagent/**/*.svg',
-		] )
+		], { base: './' } )
+		.pipe( rename( ( file ) => {
+			let myprefix = file.dirname;
+			myprefix = myprefix.replace( /.+?\/([^\\/]+)$/g, '$1' );
+
+			if ( myprefix !== 'common' && myprefix !== 'liveagent' ) {
+				myprefix = `${ myprefix }-`;
+			} else {
+				myprefix = '';
+			}
+			return {
+				dirname: file.dirname,
+				basename: `${ myprefix }${ file.basename }`,
+				extname: '.svg',
+			};
+		} ) )
 		.pipe( svgSprites( iconsConfig ) )
 		.pipe( gulp.dest( './assets/images' ) )
 		.pipe( browserSync.reload( { stream: true } ) )
