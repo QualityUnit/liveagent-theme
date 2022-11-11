7a8bb88aaac1e775892f091787c499d17b281a63
Jozef Remeň <jozef.remen@gmail.com>
2022-11-11 16:05:24 +0100

    fix: some formatting fixes to the widget

--- lib/widgets/qu-commonproblems/includes/cp-item.php
+++ lib/widgets/qu-commonproblems/includes/cp-item.php
@@ -17,12 +17,12 @@ function render_commonproblems_item( $attr ) {
 	// Always define proper Schema microdata at schema.org
 
 	return '
-		<div class="qu-enhancedFAQ__item" itemprop="mainEntity" itemscope itemtype="https://schema.org/Question">
-		<h3 data-quTarget="' . $attr['targetId'] . '" class="qu-enhancedFAQ__item--question"  itemprop="name" >
-		<svg width="18" height="18" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="9" cy="9" r="9" fill="var(--qu-faq-color-light)" /><path fill="var(--qu-faq-color)" d="m11.781 6 1.414 1.414-5.657 5.657-1.414-1.414z" /><path fill="var(--qu-faq-color)" d="M8.953 11.657 7.54 13.071 4.004 9.536l1.414-1.414z" /></svg>' . esc_html( $attr['question'] ) . '
+		<div class="qu-enhancedFAQ__item Faq__item" itemprop="mainEntity" itemscope itemtype="https://schema.org/Question">
+			<h3 data-quTarget="' . $attr['targetId'] . '" class=""  itemprop="name" >' . 
+				esc_html( $attr['question'] ) . '
 			</h3>
-			<div data-quTargetId="' . $attr['targetId'] . '" class="qu-enhancedFAQ__item--answer" itemprop="acceptedAnswer" itemscope itemtype="https://schema.org/Answer">
-			<div itemprop="text">' .
+			<div data-quTargetId="' . $attr['targetId'] . '" class="Faq__outer-wrapper" itemprop="acceptedAnswer" itemscope itemtype="https://schema.org/Answer">
+			<div class="Faq__inner-wrapper" itemprop="text">' .
 					$attr['content'] //@codingStandardsIgnoreLine
 			. '</div>
 			</div>
