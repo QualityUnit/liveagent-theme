4ed74ae0f52bd866de74de1c4bc4fd180a6409b8
Jozef Remen <jozef.remen@gmail.com>
2022-11-28 11:03:29 +0100

    fix: php memory allocation due to wp_query for whole posts content (#862)
    * fix: php memory allocation due to wp_query for whole posts content
    
    * fix: adding conditional check

---
+++ functions/get-posts.php
@@ -0,0 +1,48 @@
+<?php
+
+// Gets reviews posts to be used in reviews metaboxes
+
+function get_reviews() {
+		$query_args = array(
+			'post_type' => 'ms_reviews',
+			'posts_per_page' => -1,
+			'fields'         => 'id',
+		);
+
+		$show_posts    = new WP_Query( $query_args );
+
+		while ( $show_posts->have_posts() ) :
+			$show_posts->the_post();
+			$post_lang = apply_filters( 'wpml_post_language_details', null, get_the_id() );
+			if ( is_array( $post_lang ) && 'en' === $post_lang['language_code'] ) {
+				$reviews_posts[ get_the_id() ] = str_replace( '^', '', get_the_title() );
+			}
+		endwhile;
+
+		wp_reset_query();
+
+		return $reviews_posts;
+}
+
+	// Gets Directory posts IDs
+function get_directory_contacts() {
+	$query_args = array(
+		'post_type' => 'ms_directory',
+		'posts_per_page' => -1,
+		'fields'         => 'id',
+	);
+		
+	$show_posts    = new WP_Query( $query_args );
+
+	while ( $show_posts->have_posts() ) :
+		$show_posts->the_post();
+		$post_lang = apply_filters( 'wpml_post_language_details', null, get_the_id() );
+		if ( is_array( $post_lang ) && 'en' === $post_lang['language_code'] ) {
+			$details_posts[ get_the_id() ] = str_replace( '^', '', get_the_title() );
+		}
+		endwhile;
+
+	wp_reset_query();
+
+	return $details_posts;
+}
