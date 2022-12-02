bc9313d184fc822190b45cd6e4f071859429f566
Jozef Remeň <jozef.remen@gmail.com>
2022-12-02 09:21:14 +0100

    fix: coauthor image size and lazyload

--- lib/shortcodes/author.php
+++ lib/shortcodes/author.php
@@ -15,7 +15,7 @@ function ms_author( $atts ) {
 
 	<div class="BlogPost__co-author">
 		<div class="BlogPost__co-author__image">
-			<img src="<?= esc_url( $atts['photo'] ); ?>" alt="<?= esc_attr( $atts['name'] ); ?>">
+			<img data-src="<?= esc_url( $atts['photo'] ); ?>" alt="<?= esc_attr( $atts['name'] ); ?>">
 		</div>
 		<div class="BlogPost__co-author__content">
 			<p class="BlogPost__co-author__content__title"><?php _e( 'Guest Post Author', 'ms' ); ?></p>
