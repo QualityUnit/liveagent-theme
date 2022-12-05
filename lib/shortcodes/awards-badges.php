40f4d509c87a97a881dd76198b6056b8c5f0fcb2
Rastislav Laco <106659050+rlaco-qualityunit@users.noreply.github.com>
2022-12-05 14:51:57 +0100

    [934] Home redesign feedback (#880)
    * fix: homepage feedback changes
    
    * fix: awards badges instead of gutenberg awards
    
    * fix: awards badges instead of gutenberg awards

---
+++ lib/shortcodes/awards-badges.php
@@ -0,0 +1,35 @@
+<?php
+
+function ms_awards_badges() {
+	ob_start();
+	?>
+ 
+	<section class="block-badges">
+		<div class="block-badges__bg">
+			<div class="block-badges__wrap">
+				<div class="block-badges__content">
+					<h5 class="block-badges__subtitle">
+						<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/Awards.svg" alt="<?php _e( 'Awards', 'ms' ); ?>">
+					</h5>
+					<h2 class="block-badges__title h2 larger">
+						<?php _e( 'Happy customers are <span class="highlight">the best</span> customers', 'ms' ); ?>
+					</h2>
+					<p>
+						<?php _e( 'We offer concierge migration services from most of the popular help desk solutions.', 'ms' ); ?>
+					</p>
+					<p>
+						<a href="<?= esc_url( __( '/awards/', 'ms' ) ); ?>" class="Button Button--full"><span><?php _e( 'View more' ); ?></span></a>
+					</p>
+				</div>
+				<div class="block-badges__image">
+					<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/badges.png" alt="<?php _e( 'Badges', 'ms' ); ?>">
+				</div>
+			</div>
+		</div>
+	</section>
+ 
+	<?php
+	set_custom_source( 'shortcodes/BlockBadges' );
+	return ob_get_clean();
+}
+add_shortcode( 'awards-badges', 'ms_awards_badges' );
