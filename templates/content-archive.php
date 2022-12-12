1b3e9da76d3239e1a14739476c37f8909c0d3dcf
Jozef Remeň <jozef.remen@gmail.com>
2022-12-12 09:04:21 +0100

    fix: archive php bug

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -137,7 +137,7 @@
 					'no_found_rows'       => true,
 				);
 
-        if ( $this_category && 0 != $this_category->parent ) { // @codingStandardsIgnoreLine
+        if ( ( $this_category && isset ( $this_category->parent ) ) && 0 != $this_category->parent ) { // @codingStandardsIgnoreLine
 					$query_args = array(
 						'ignore_sticky_posts' => true,
 						'posts_per_page'      => 9,
