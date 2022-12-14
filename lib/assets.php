1793c5bef2be02a683e8e1382c53273a3d0887d7
Martin Stepanek <martin@stepanek.sk>
2022-12-14 14:15:42 +0100

    feat: improvements in loading of fonts (#908)

--- lib/assets.php
+++ lib/assets.php
@@ -31,12 +31,10 @@ add_action( 'enqueue_block_editor_assets', 'editor_assets' );
 add_action(
 	'wp_enqueue_scripts',
 	function () {
-		wp_enqueue_style( 'la-font-poppins', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap', false, '1' );
-		wp_enqueue_style( 'la-font-opensans', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap', false, '1' );
-		wp_enqueue_style( 'la-font-patrickhand', 'https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap', false, '1' );
+		wp_enqueue_style( 'la-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Patrick+Hand&family=Poppins:wght@400;700&display=swap', false, '1' );
 
 		wp_enqueue_style( 'header', get_template_directory_uri() . '/assets/dist/layouts/Header' . isrtl() . wpenv() . '.css', false, THEME_VERSION );
-		
+
 		if ( is_page_template( 'elementor.php' ) || is_page_template( 'front-page.php' ) || is_page_template( 'page.php' ) || is_page_template( 'template-academy-header.php' ) || is_page_template( 'template-blog-header.php' ) ) {
 			wp_enqueue_style( 'elementor-layout', get_template_directory_uri() . '/assets/dist/Elementor' . isrtl() . wpenv() . '.css', false, THEME_VERSION );
 		}
