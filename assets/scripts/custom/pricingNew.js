3953bfc699eca35559c920e2131990f50c90dd34
Jozef Remeň <jozef.remen@gmail.com>
2023-01-02 08:08:26 +0100

    feat: new pricing

--- assets/scripts/custom/pricingNew.js
+++ assets/scripts/custom/pricingNew.js
@@ -1,7 +1,42 @@
 const comparePlansTableTitles = document.querySelectorAll( '.ComparePlans .elementor-post__title' );
 const comparePlansTitle = document.querySelector( '.ComparePlans__sectionTitle' );
 const comparePlansHeader = document.querySelector( '.ComparePlans__header' );
+const comparePlansTables = document.querySelectorAll( '.ComparePlans table' );
 const comparePlansRows = document.querySelectorAll( '.ComparePlans table tr' );
+
+const setOddEven = () => {
+	if ( comparePlansRows.length ) {
+		comparePlansRows.forEach( ( row ) => {
+			row.classList.remove( 'even' );
+		} );
+	}
+
+	comparePlansTables.forEach( ( table ) => {
+		const visibleRows = table.querySelectorAll( 'tr:not(.hidden)' );
+
+		if ( visibleRows.length ) {
+			visibleRows.forEach( ( element, index ) => {
+				const item = element;
+				if ( index % 2 !== 0 ) { //We start at 0 not 1 so even is with modulus
+					item.classList.add( 'even' );
+				}
+			} );
+		}
+	} );
+};
+
+// const comparePlansTableTitlesObserver = new IntersectionObserver(
+// 	( [ title ] ) => {
+// 		if ( title.isIntersecting ) {
+// 			title.target.classList.remove( 'is-sticky' );
+// 			return;
+// 		}
+// 		if ( title.boundingClientRect.top < 0 ) {
+// 			title.target.classList.add( 'is-sticky' );
+// 		}
+// 	},
+// 	{ rootMargin: '0px 0px -1000px 0px', threshold: 1 }
+// );
 
 // Setting tables header class when sticky to hide icons
 if ( comparePlansHeader ) {
@@ -13,6 +48,9 @@ if ( comparePlansHeader ) {
 			}
 			if ( entry.boundingClientRect.top < 0 ) {
 				comparePlansHeader.classList.add( 'is-sticky' );
+				// document.querySelectorAll( '.elementor-post.ms_pricing' ).forEach( ( table ) => {
+				// 	comparePlansTableTitlesObserver.observe( table );
+				// } );
 			}
 		},
 		{ rootMargin: '-92px 0px 0px 0px' }
@@ -33,6 +71,9 @@ if ( comparePlansTableTitles.length ) {
 // Finding differences in values in pricing table rows
 if ( comparePlansRows.length ) {
 	const matchingRows = [];
+	const compareSwitcher = document.querySelector( '[data-switcher="compare"]' );
+	const compareSwitcherLabels = compareSwitcher.querySelectorAll( 'label' );
+	setOddEven();
 
 	comparePlansRows.forEach( ( row ) => {
 		// Selecting cells except first description one
@@ -47,4 +88,19 @@ if ( comparePlansRows.length ) {
 			matchingRows.push( row );
 		}
 	} );
+
+	if ( matchingRows.length ) {
+		compareSwitcher.addEventListener( 'click', () => {
+			compareSwitcherLabels.forEach( ( label ) => {
+				label.classList.toggle( 'active' );
+			} );
+			matchingRows.forEach( ( row ) => {
+				row.classList.toggle( 'hidden' );
+			} );
+			setOddEven();
+		} );
+	}
+	if ( ! matchingRows.length ) {
+		compareSwitcher.classList.add( 'inactive' );
+	}
 }
