2b55cd2113a72d8c72c0f6b8d99a9cd34e1470b3
Jozef Remeň <jozef.remen@gmail.com>
2023-01-11 09:45:50 +0100

    fix: hiding post type from public

--- lib/post-types/pricing-tables.php
+++ lib/post-types/pricing-tables.php
@@ -11,8 +11,8 @@ add_action(
 		);
 		$rewrite = array(
 			'slug'       => 'pricing-table',
-			'with_front' => true,
-			'pages'      => true,
+			'with_front' => false,
+			'pages'      => false,
 			'feeds'      => false,
 		);
 		$args    = array(
@@ -28,9 +28,9 @@ add_action(
 			'show_in_admin_bar'   => true,
 			'show_in_nav_menus'   => true,
 			'can_export'          => true,
-			'has_archive'         => true,
+			'has_archive'         => false,
 			'exclude_from_search' => false,
-			'publicly_queryable'  => true,
+			'publicly_queryable'  => false,
 			'rewrite'             => $rewrite,
 			'capability_type'     => 'post',
 			'show_in_rest'        => true,
