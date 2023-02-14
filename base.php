ef90876b97f692359b5bd025b18b14802312c6c1
Jozef Remeň <jozef.remen@gmail.com>
2023-02-14 10:32:09 +0100

    fix: change of new pricing resources reference paths

--- base.php
+++ base.php
@@ -6,6 +6,7 @@
 	<?php get_template_part( 'templates/head' ); ?>
 	<body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
+	<?php print_r('kokot' . get_permalink()); ?>
 
 	<div id="app">
 		<?php
