a9eb93f520cce7f152b8ad2a5efc1feab43507de
Jozef Remeň <jozef.remen@gmail.com>
2023-02-20 10:02:16 +0100

    fix: splitting code

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -1,4 +1,3 @@
-/* global Popper */
 ( () => {
 	const mobileTablet = window.matchMedia( '(max-width: 1380px)' );
 	const query = document.querySelector.bind( document );
@@ -390,75 +389,6 @@
 					)
 					.classList.add( 'active' );
 			} );
-		} );
-	}
-
-	/* Tooltips */
-	tooltips( [ 'Tooltip', 'ComparePlans__tooltip' ] );
-	function tooltips( classList = [] ) {
-		classList.forEach( ( className ) => {
-			if ( queryAll( '.' + className ).length > 0 && typeof Popper !== 'undefined' ) {
-				queryAll( '.' + className ).forEach( ( trigger ) => {
-					const classText = className + '__text';
-					const tooltip = trigger.querySelector( '.' + classText );
-					if ( tooltip ) {
-						let defaultPlacement = 'bottom';
-						const arrow = document.createElement( 'span' );
-						arrow.setAttribute( 'data-popper-arrow', '' );
-						tooltip.appendChild( arrow );
-
-						if ( tooltip.classList.contains( classText + '--top' ) ) {
-							defaultPlacement = 'top';
-						}
-
-						const popperInstance = Popper.createPopper( trigger, tooltip, {
-							placement: defaultPlacement,
-							modifiers: [
-								{
-									name: 'offset',
-									options: {
-										offset: [ 0, 8 ],
-									},
-								},
-							],
-						} );
-
-						function show() {
-							tooltip.setAttribute( 'data-show', '' );
-							popperInstance.setOptions( ( options ) => ( {
-								...options,
-								modifiers: [
-									...options.modifiers,
-									{ name: 'eventListeners', enabled: true },
-								],
-							} ) );
-							popperInstance.update();
-						}
-
-						function hide() {
-							tooltip.removeAttribute( 'data-show' );
-							popperInstance.setOptions( ( options ) => ( {
-								...options,
-								modifiers: [
-									...options.modifiers,
-									{ name: 'eventListeners', enabled: false },
-								],
-							} ) );
-						}
-
-						const showEvents = [ 'mouseenter', 'focus' ];
-						const hideEvents = [ 'mouseleave', 'blur' ];
-
-						showEvents.forEach( ( event ) => {
-							trigger.addEventListener( event, show );
-						} );
-
-						hideEvents.forEach( ( event ) => {
-							trigger.addEventListener( event, hide );
-						} );
-					}
-				} );
-			}
 		} );
 	}
 } )();
