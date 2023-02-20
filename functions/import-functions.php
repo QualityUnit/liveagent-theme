e96c525817dc4e9dc56e08e970b9e65a5af503cf
Rastislav Laco <106659050+rlaco-qualityunit@users.noreply.github.com>
2023-02-20 09:40:39 +0100

    [874] Breadcrumb and Schema (#983)
    * feat: breadcrumb
    
    * feat: breadcrumbs schema and html
    
    * feat: breadcrumbs function and styles
    
    * fix: breadcrumbs function code
    
    * feat: breadcrumb microdata

--- functions/import-functions.php
+++ functions/import-functions.php
@@ -47,3 +47,110 @@ function set_custom_source( $source_file, $filetype = 'css', $depends = false ) 
 		wp_enqueue_script( $source_file, get_template_directory_uri() . '/assets/dist/' . $source_file . wpenv() . '.js', $depends, THEME_VERSION, true );
 	}
 }
+
+function is_subcategory() {
+	$cat = get_query_var( 'cat' );
+	$category = get_category( $cat );
+	return ! ( ( '0' == $category->parent ) );
+}
+
+// Breadcrumb (BreadcrumbList) structured data
+function site_breadcrumb() {
+	$breadcrumb = array();
+	$home = array( __( 'Home' ), home_url( '/', 'relative' ) );
+	
+	if ( is_single() ) {
+		$post_type = get_post_type_object( get_post_type() );
+		if ( $post_type ) {
+			$post_type_name = $post_type->labels->name;
+			if ( 'Posts' == $post_type_name ) {
+				$categories = get_the_category();
+				$cat = $categories[0];
+				if ( $cat ) {
+					$breadcrumb[] = array( $cat->name, get_category_link( $cat->term_id ) );
+				}
+			} else {
+				$post_type_url = get_post_type_archive_link( $post_type->name );
+				$breadcrumb[] = array( $post_type_name, $post_type_url );
+			}
+		}
+		$breadcrumb[] = array( get_the_title() );
+	} elseif ( is_category() ) {
+		$cat = get_the_category()[0];
+		if ( is_subcategory() ) {
+			if ( $cat ) {
+				$breadcrumb[] = array( $cat->name, get_category_link( $cat->term_id ) );
+			}
+		} else {
+			$breadcrumb[] = $home;
+		}
+		$breadcrumb[] = array( single_cat_title( '', false ) );
+	} elseif ( is_archive() && ! is_category() ) {
+		$breadcrumb[] = $home;
+		$post_type = get_queried_object();
+		if ( $post_type ) {
+			$breadcrumb[] = array( $post_type->labels->name );
+		}
+	} elseif ( get_post_type() === '' ) {
+		$breadcrumb[] = $home;
+	} elseif ( is_front_page() ) {
+		$breadcrumb[] = $home;
+	} elseif ( is_page() ) {
+		$breadcrumb[] = $home;
+		$breadcrumb[] = array( get_the_title() );
+	} elseif ( is_search() ) {
+		$breadcrumb[] = $home;
+		$breadcrumb[] = array( get_search_query() );
+	}
+ 
+	$allowed_html = array(
+		'div' => array(
+			'class' => array(),
+		),
+		'ol' => array(
+			'itemscope' => array(),
+			'itemtype' => array(),
+		),
+		'li' => array(
+			'itemprop' => array(),
+			'itemscope' => array(),
+			'itemtype' => array(),
+		),
+		'a' => array(
+			'itemscope' => array(),
+			'itemtype' => array(),
+			'itemprop' => array(),
+			'itemid' => array(),
+			'href' => array(),
+		),
+		'span' => array(
+			'itemprop' => array(),
+		),
+		'meta' => array(
+			'itemprop' => array(),
+			'content' => array(),
+		),
+	);
+	$output = '';
+	$i = 1;
+	$output .= '<div class="breadcrumbs">';
+	$output .= '<ol itemscope itemtype="https://schema.org/BreadcrumbList">';
+	foreach ( $breadcrumb as $item ) {
+		$item_name = $item[0];
+		$output .= '<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">';
+		if ( count( $item ) == 2 ) {
+			$item_url = $item[1];
+			$output .= '<a itemscope itemtype="https://schema.org/WebPage" itemprop="item" itemid="' . $item_url . '" href="' . $item_url . '">';
+		}
+		$output .= '<span itemprop="name">' . $item_name . '</span>';
+		if ( count( $item ) == 2 ) {
+			$output .= '</a>';
+		}
+		$output .= '<meta itemprop="position" content="' . $i . '" />';
+		$output .= '</li>';
+		$i++;
+	}
+	$output .= '</ol>';
+	$output .= '</div>';
+	echo wp_kses( $output, $allowed_html );
+}
