2e329d50c27394d8611f28782d44552344c9a387
Martin Stepanek <martin@stepanek.sk>
2023-04-11 11:19:41 +0200

    fix: temp disable show and hide custom fields

--- assets/scripts/custom/editor.js
+++ assets/scripts/custom/editor.js
@@ -1,16 +1,16 @@
-window.addEventListener( 'load', () => {
-	setTimeout( () => {
-		const metaboxes = document.querySelector( '.edit-post-layout__metaboxes' );
-		const editHeader = document.querySelector( '.edit-post-header' );
-		const showMetaboxButton = document.createElement( 'button' );
-		showMetaboxButton.classList.add( 'showMetaboxes' );
-		showMetaboxButton.innerText = 'Show/hide Custom Fields';
-
-		if ( metaboxes ) {
-			editHeader.insertAdjacentElement( 'afterbegin', showMetaboxButton );
-			showMetaboxButton.addEventListener( 'click', ( ) => {
-				metaboxes.classList.toggle( 'visible' );
-			} );
-		}
-	}, 0 );
-} );
+// window.addEventListener( 'load', () => {
+// 	setTimeout( () => {
+// 		const metaboxes = document.querySelector( '.edit-post-layout__metaboxes' );
+// 		const editHeader = document.querySelector( '.edit-post-header' );
+// 		const showMetaboxButton = document.createElement( 'button' );
+// 		showMetaboxButton.classList.add( 'showMetaboxes' );
+// 		showMetaboxButton.innerText = 'Show/hide Custom Fields';
+//
+// 		if ( metaboxes ) {
+// 			editHeader.insertAdjacentElement( 'afterbegin', showMetaboxButton );
+// 			showMetaboxButton.addEventListener( 'click', ( ) => {
+// 				metaboxes.classList.toggle( 'visible' );
+// 			} );
+// 		}
+// 	}, 0 );
+// } );
