d77e960782ff6fcd3c4db0fabe2934369d56d33c
thujcak <thujcak@qualityunit.com>
2023-06-13 09:06:00 +0200

    fix: try fix errors in php

--- templates/content-archive-ms_features.php
+++ templates/content-archive-ms_features.php
@@ -1,255 +1,261 @@
 <?php // @codingStandardsIgnoreLine
-set_source('features', 'pages/Category', 'css');
-set_source('features', 'pages/CategoryImages', 'css');
-set_source('features', 'pages/CategoryLabelColors', 'css');
-set_source('features', 'filter', 'js');
-$categories = array_unique(get_categories([ 'taxonomy' => 'ms_features_categories' ]), SORT_REGULAR);
-if (is_tax('ms_features_categories') === true) :
-    $pageHeaderTitle       = single_cat_title();
-    $pageHeaderDescription = the_archive_description();
+set_source( 'features', 'pages/Category', 'css' );
+set_source( 'features', 'pages/CategoryImages', 'css' );
+set_source( 'features', 'pages/CategoryLabelColors', 'css' );
+set_source( 'features', 'filter', 'js' );
+$categories = array_unique( get_categories( array( 'taxonomy' => 'ms_features_categories' ) ), SORT_REGULAR );
+if ( is_tax( 'ms_features_categories' ) === true ) :
+	$page_header_title       = single_cat_title();
+	$page_header_description = the_archive_description();
 else :
-    $pageHeaderTitle       = __('Features', 'ms');
-    $pageHeaderDescription = __('Get to know all LiveAgent features, that are part of the complex multi-channel help desk software. Described in one place and in depth.', 'ms');
+	$page_header_title       = __( 'Features', 'ms' );
+	$page_header_description = __( 'Get to know all LiveAgent features, that are part of the complex multi-channel help desk software. Described in one place and in depth.', 'ms' );
 endif;
-$filterItemsCategories = [
-    [
-        'checked' => true,
-        'value'   => '',
-        'title'   => __('Any', 'ms'),
-    ],
-];
-foreach ($categories as $category) :
-    $filterItemsCategories[] = [
-        'value' => $category->slug,
-        'title' => $category->name,
-    ];
+$filter_items_categories = array(
+	array(
+		'checked' => true,
+		'value'   => '',
+		'title'   => __( 'Any', 'ms' ),
+	),
+);
+foreach ( $categories as $category ) :
+	$filter_items_categories[] = array(
+		'value' => $category->slug,
+		'title' => $category->name,
+	);
 endforeach;
-$filterItems    = [
-    [
-        'type'  => 'radio',
-        'name'  => 'collections',
-        'title' => __('Collections', 'ms'),
-        'items' => [
-            [
-                'checked' => true,
-                'value'   => '',
-                'title'   => __('Any', 'ms'),
-            ],
-            [
-                'value' => 'featured',
-                'title' => __('Featured', 'ms'),
-            ],
-            [
-                'value' => 'popular',
-                'title' => __('Popular', 'ms'),
-            ],
-            [
-                'value' => 'new',
-                'title' => __('New', 'ms'),
-            ],
-        ],
-    ],
-    [
-        'type'  => 'radio',
-        'name'  => 'plan',
-        'title' => __('Available in', 'ms'),
-        'items' => [
-            [
-                'checked' => true,
-                'value'   => '',
-                'title'   => __('Any', 'ms'),
-            ],
-            [
-                'value' => 'free',
-                'title' => __('Free', 'ms'),
-            ],
-            [
-                'value' => 'ticket',
-                'title' => __('Small', 'ms'),
-            ],
-            [
-                'value' => 'ticket-chat',
-                'title' => __('Medium', 'ms'),
-            ],
-            [
-                'value' => 'all-inclusive',
-                'title' => __('Large', 'ms'),
-            ],
-            [
-                'value' => 'extensions',
-                'title' => __('Extensions', 'ms'),
-            ],
-        ],
-    ],
-    [
-        'type'  => 'radio',
-        'name'  => 'size',
-        'title' => __('Suitable for', 'ms'),
-        'items' => [
-            [
-                'checked' => true,
-                'value'   => '',
-                'title'   => __('Any', 'ms'),
-            ],
-            [
-                'value' => 'individuals',
-                'title' => __('Individuals', 'ms'),
-            ],
-            [
-                'value' => 'start-ups',
-                'title' => __('Start-ups', 'ms'),
-            ],
-            [
-                'value' => 'smbs',
-                'title' => __('SMBs', 'ms'),
-            ],
-            [
-                'value' => 'enterprise',
-                'title' => __('Enterprise', 'ms'),
-            ],
-        ],
-    ],
-    [
-        'type'  => 'radio',
-        'name'  => 'category',
-        'title' => __('Category', 'ms'),
-        'items' => $filterItemsCategories,
-    ],
-];
-$pageHeaderArgs = [
-    'type'   => 'lvl-1',
-    'image'  => [
-        'src' => get_template_directory_uri().'/assets/images/compact_header_features.png?ver='.THEME_VERSION,
-        'alt' => $pageHeaderTitle,
-    ],
-    'title'  => $pageHeaderTitle,
-    'text'   => $pageHeaderDescription,
-    'filter' => $filterItems,
-    'search' => ['type' => 'academy'],
-];
+$filter_items     = array(
+	array(
+		'type'  => 'radio',
+		'name'  => 'collections',
+		'title' => __( 'Collections', 'ms' ),
+		'items' => array(
+			array(
+				'checked' => true,
+				'value'   => '',
+				'title'   => __( 'Any', 'ms' ),
+			),
+			array(
+				'value' => 'featured',
+				'title' => __( 'Featured', 'ms' ),
+			),
+			array(
+				'value' => 'popular',
+				'title' => __( 'Popular', 'ms' ),
+			),
+			array(
+				'value' => 'new',
+				'title' => __( 'New', 'ms' ),
+			),
+		),
+	),
+	array(
+		'type'  => 'radio',
+		'name'  => 'plan',
+		'title' => __( 'Available in', 'ms' ),
+		'items' => array(
+			array(
+				'checked' => true,
+				'value'   => '',
+				'title'   => __( 'Any', 'ms' ),
+			),
+			array(
+				'value' => 'free',
+				'title' => __( 'Free', 'ms' ),
+			),
+			array(
+				'value' => 'ticket',
+				'title' => __( 'Small', 'ms' ),
+			),
+			array(
+				'value' => 'ticket-chat',
+				'title' => __( 'Medium', 'ms' ),
+			),
+			array(
+				'value' => 'all-inclusive',
+				'title' => __( 'Large', 'ms' ),
+			),
+			array(
+				'value' => 'extensions',
+				'title' => __( 'Extensions', 'ms' ),
+			),
+		),
+	),
+	array(
+		'type'  => 'radio',
+		'name'  => 'size',
+		'title' => __( 'Suitable for', 'ms' ),
+		'items' => array(
+			array(
+				'checked' => true,
+				'value'   => '',
+				'title'   => __( 'Any', 'ms' ),
+			),
+			array(
+				'value' => 'individuals',
+				'title' => __( 'Individuals', 'ms' ),
+			),
+			array(
+				'value' => 'start-ups',
+				'title' => __( 'Start-ups', 'ms' ),
+			),
+			array(
+				'value' => 'smbs',
+				'title' => __( 'SMBs', 'ms' ),
+			),
+			array(
+				'value' => 'enterprise',
+				'title' => __( 'Enterprise', 'ms' ),
+			),
+		),
+	),
+	array(
+		'type'  => 'radio',
+		'name'  => 'category',
+		'title' => __( 'Category', 'ms' ),
+		'items' => $filter_items_categories,
+	),
+);
+$page_header_args = array(
+	'type'   => 'lvl-1',
+	'image'  => array(
+		'src' => get_template_directory_uri() . '/assets/images/compact_header_features.png?ver=' . THEME_VERSION,
+		'alt' => $page_header_title,
+	),
+	'title'  => $page_header_title,
+	'text'   => $page_header_description,
+	'filter' => $filter_items,
+	'search' => array( 'type' => 'academy' ),
+);
 ?>
 
 <div id="category" class="Category">
-    <?php get_template_part('lib/custom-blocks/compact-header', null, $pageHeaderArgs); ?>
+	<?php get_template_part( 'lib/custom-blocks/compact-header', null, $page_header_args ); ?>
 
-    <div class="wrapper Category__container">
-        <div class="Category__content">
-            <ul class="Category__content__items list">
-                <?php
-                while (have_posts() === true) :
-                    the_post();
-                    ?>
+	<div class="wrapper Category__container">
+		<div class="Category__content">
+			<ul class="Category__content__items list">
+				<?php
+				while ( have_posts() === true ) :
+					the_post();
+					?>
 
-                    <?php
-                    $collections = '';
-                    $plan        = '';
-                    $size        = '';
-                    $category    = '';
+					<?php
+					$collections = '';
+					$plan        = '';
+					$size        = '';
+					$category    = '';
 
-                    $futurePlans       = (get_post_meta(get_the_ID(), 'mb_features_mb_features_plan', true) ?? '');
-                    $futureSizes       = (get_post_meta(get_the_ID(), 'mb_features_mb_features_size', true) ?? '') ;
-                    $futureCollections = (get_post_meta(get_the_ID(), 'mb_features_mb_features_collections', true) ?? '');
-
-
-                    if ($futurePlans === true) {
-                        $plan = implode(' ', $futurePlans);
-                    }
-
-                    if ($futureSizes === true) {
-                        $size = implode(' ', $futureSizes);
-                    }
-
-                    if ($futureCollections === true) {
-                        $collections = implode(' ', $futureCollections);
-                    }
+					$future_plans       = ( get_post_meta( get_the_ID(), 'mb_features_mb_features_plan', true ) ?? '' );
+					$future_sizes       = ( get_post_meta( get_the_ID(), 'mb_features_mb_features_size', true ) ?? '' );
+					$future_collections = ( get_post_meta( get_the_ID(), 'mb_features_mb_features_collections', true ) ?? '' );
 
 
-                    $categories  = get_the_terms(0, 'ms_features_categories');
-                    $currentLang = apply_filters('wpml_current_language', null);
-                    do_action('wpml_switch_language', 'en');
-                    $categoriesEn = get_the_terms(0, 'ms_features_categories');
-                    if (empty($categoriesEn) === false) {
-                        $categoryEn = array_shift($categoriesEn)->slug;
-                    }
+					if (true === $future_plans ) {
+						$plan = implode( ' ', $future_plans );
+					}
 
-                    do_action('wpml_switch_language', $currentLang);
-                    if (empty($categories) === false) {
-                        foreach ($categories as $categoryItem) {
-                            $categoryItem = array_shift($categories);
-                            $category    .= $categoryItem->slug;
-                            $category    .= ' ';
-                        }
-                    }
+					if (true === $future_sizes ) {
+						$size = implode( ' ', $future_sizes );
+					}
 
-                    $category = substr($category, 0, -1);
+					if ( true === $future_collections ) {
+						$collections = implode( ' ', $future_collections );
+					}
 
-                    ?>
 
-                    <?php
-                    // Element classes.
-                    $pillarValue = (get_post_meta(get_the_ID(), 'mb_features_mb_features_pillar', true) ?? '');
-                    $pillarClass = '';
-                    if ($pillarValue === 'on') {
-                            $pillarClass = 'pillar';
-                    }
+					$categories   = get_the_terms( 0, 'ms_features_categories' );
+					$current_lang = apply_filters( 'wpml_current_language', null );
+					do_action( 'wpml_switch_language', 'en' );
+					$categories_en = get_the_terms( 0, 'ms_features_categories' );
+					if ( ! empty( $categories_en ) ) {
+						$category_en = array_shift( $categories_en )->slug;
+					}
 
-                    $categoryItemClasses = 'Category__item redesign Category__item--features '.$pillarClass.' '.esc_attr($category).' '.esc_attr($categoryEn);
+					do_action( 'wpml_switch_language', $current_lang );
+					if ( ! empty( $categories ) ) {
+						foreach ( $categories as $category_item ) {
+							$category_item = array_shift( $categories );
+							$category     .= $category_item->slug;
+							$category     .= ' ';
+						}
+					}
 
-                    // Element attributes.
-                    $categoryItemAttributes = [
-                        'data-plan'        => esc_attr($plan),
-                        'data-collections' => esc_attr($collections),
-                        'data-size'        => esc_attr($size),
-                        'data-category'    => esc_attr($category),
-                        'data-href'        => get_permalink(),
-                    ];
-                    if ($pillarValue === 'on') : ?>
-                            <li class="<?php echo $categoryItemClasses ?>" <?php foreach ($categoryItemAttributes as $name => $value) {
-                                echo $name.'="'.$value.'" ';
-                                       } ?>>
-                            <a href="<?php the_permalink(); ?>" class="Category__item__thumbnail">
-                                <span class="Category__item__thumbnail__image"></span>
-                            </a>
-                            <div class="Category__item__wrap">
-                                <h3 class="Category__item__title item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                                <div class="Category__item__excerpt item-excerpt">
-                                    <a href="<?php the_permalink(); ?>">
-                        <?php echo esc_html(wp_trim_words(get_the_excerpt(), 14)); ?>
-                                    </a>
-                                </div>
-                                <a class="Category__item__cta" href="<?php the_permalink(); ?>"><?php _e('Learn more', 'ms'); ?></a>
-                            </div>
-                            </li>
-        <?php else : ?>
-                            <li class="<?php echo $categoryItemClasses ?>" <?php foreach ($categoryItemAttributes as $name => $value) {
-                                echo $name.'="'.$value.'" ';
-}?>>
-                                <div class="Category__item__wrap">
-                                    <div class="Category__item__header">
-            <?php
-            if (has_post_thumbnail() === true) {
-                the_post_thumbnail('archive_thumbnail');
-            } else { ?>
-                                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/icon-custom-post_type.svg" alt="<?php _e('Features', 'ms'); ?>">
-            <?php	} ?>
-                                        <div class="Category__item__header__label">
-                                            <span class="Category__item__header__label_text">Ticketing system</span>
-                                        </div>
-                                    </div>
-                                    <div class="Category__item__content">
-                                        <h3 class="Category__item__title item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                                        <div class="Category__item__excerpt item-excerpt">
-                                            <a href="<?php the_permalink(); ?>">
-                                                <?php echo esc_html(wp_trim_words(get_the_excerpt(), 14)); ?>
-                                            </a>
-                                        </div>
-                                        <a class="Category__item__cta" href="<?php the_permalink(); ?>"><?php _e('Learn more', 'ms'); ?></a>
-                                    </div>
-                                </div>
-                            </li>
-        <?php endif; ?>
-                <?php endwhile; ?>
-            </ul>
-        </div>
-    </div>
+					$category = substr( $category, 0, -1 );
+
+					?>
+
+					<?php
+					// Element classes
+					$pillar_value          = ( get_post_meta( get_the_ID(), 'mb_features_mb_features_pillar', true ) ?? '' );
+					$pillar_class          = $pillar_value === 'on' ? 'pillar ' : '';
+					$category_item_classes = 'Category__item redesign Category__item--features ' . $pillar_class . ' ' . esc_attr( $category ) . ' ' . esc_attr( $category_en );
+
+					// Element attributes
+					$category_item_attributes = array(
+						'data-plan'        => esc_attr( $plan ),
+						'data-collections' => esc_attr( $collections ),
+						'data-size'        => esc_attr( $size ),
+						'data-category'    => esc_attr( $category ),
+						'data-href'        => get_permalink(),
+					);
+					if ( $pillar_value === 'on' ) :
+						?>
+							<li class="<?php echo $category_item_classes; ?>"
+												  <?php
+													foreach ( $category_item_attributes as $name => $value ) {
+														echo $name . '="' . $value . '" ';
+													}
+													?>
+									   >
+							<a href="<?php the_permalink(); ?>" class="Category__item__thumbnail">
+								<span class="Category__item__thumbnail__image"></span>
+							</a>
+							<div class="Category__item__wrap">
+								<h3 class="Category__item__title item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+								<div class="Category__item__excerpt item-excerpt">
+									<a href="<?php the_permalink(); ?>">
+						<?php echo esc_html( wp_trim_words( get_the_excerpt(), 14 ) ); ?>
+									</a>
+								</div>
+								<a class="Category__item__cta" href="<?php the_permalink(); ?>"><?php _e( 'Learn more', 'ms' ); ?></a>
+							</div>
+							</li>
+		<?php else : ?>
+							<li class="<?php echo $category_item_classes; ?>"
+												  <?php
+													foreach ( $category_item_attributes as $name => $value ) {
+														echo $name . '="' . $value . '" ';
+													}
+													?>
+									   >
+								<div class="Category__item__wrap">
+									<div class="Category__item__header">
+			<?php
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail( 'archive_thumbnail' );
+			} else {
+				?>
+												<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/icon-custom-post_type.svg" alt="<?php _e( 'Features', 'ms' ); ?>">
+			<?php	} ?>
+										<div class="Category__item__header__label">
+											<span class="Category__item__header__label_text">Ticketing system</span>
+										</div>
+									</div>
+									<div class="Category__item__content">
+										<h3 class="Category__item__title item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+										<div class="Category__item__excerpt item-excerpt">
+											<a href="<?php the_permalink(); ?>">
+												<?php echo esc_html( wp_trim_words( get_the_excerpt(), 14 ) ); ?>
+											</a>
+										</div>
+										<a class="Category__item__cta" href="<?php the_permalink(); ?>"><?php _e( 'Learn more', 'ms' ); ?></a>
+									</div>
+								</div>
+							</li>
+		<?php endif; ?>
+				<?php endwhile; ?>
+			</ul>
+		</div>
+	</div>
 </div>
