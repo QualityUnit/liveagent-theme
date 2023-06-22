ca33e104735278a45000d8783829f8dd0310280f
Rastislav Laco <106659050+rlaco-qualityunit@users.noreply.github.com>
2023-06-22 07:07:39 +0200

    [1211] Compact header on SEO pages (#1320)
    * feat: compact header in blog header template
    
    * fix: compact header image check

--- template-blog-header.php
+++ template-blog-header.php
@@ -13,15 +13,17 @@
 	global $post;
 	$page_title = str_replace( '^', '', get_the_title() );
 	$page_header_args = array(
-		'image' => array(
-			'type' => 'main',
-			'src' => get_the_post_thumbnail_url( $post, 'blog_post_thumbnail' ),
-			'alt' => $page_title,
-		),
 		'title' => $page_title,
 		'text' => do_shortcode( '[urlslab-generator id="6"]' ),
 		'toc' => true,
 	);
+	if ( has_post_thumbnail() ) {
+		$page_header_args['image'] = array(
+			'type' => 'main',
+			'src' => get_the_post_thumbnail_url( $post, 'blog_post_thumbnail' ),
+			'alt' => $page_title,
+		);
+	}
 	?>
 
 <?php get_template_part( 'lib/custom-blocks/compact-header', null, $page_header_args ); ?>
