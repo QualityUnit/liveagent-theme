617316cbd387bc501fba680a23dd5c8a5768af48
Jozef Remen <jozef.remen@gmail.com>
2023-08-23 07:56:25 +0200

    fix: 1888 redesign video section (#1499)

--- templates/content-archive-ms_videos.php
+++ templates/content-archive-ms_videos.php
@@ -1,6 +1,17 @@
 <?php // @codingStandardsIgnoreLine
 set_source( 'videos', 'pages/Category', 'css' );
 set_source( 'videos', 'filter', 'js' );
+
+function duration_to_time( $youtube_time ) {
+	if ( $youtube_time ) {
+		$start = new DateTime( '@0' ); // Unix epoch
+		$start->add( new DateInterval( $youtube_time ) );
+		$youtube_time = ltrim( ltrim( $start->format( 'H:i:s' ), '0' ), ':' );
+	}
+
+	return $youtube_time;
+}
+
 $categories = array_unique( get_categories( array( 'taxonomy' => 'ms_videos_categories' ) ), SORT_REGULAR );
 if ( is_tax( 'ms_videos_categories' ) ) :
 	$page_header_title       = single_cat_title();
@@ -73,16 +84,18 @@ $page_header_args = array(
 					$category = substr( $category, 0, -1 );
 					?>
 
-					<li class="Category__item Category__item--videos" data-category="<?= esc_attr( $category ); ?>" data-href="<?php the_permalink(); ?>">
+					<li class="Category__item Category__item--videos Category__item--blogLike" data-category="<?= esc_attr( $category ); ?>" data-href="<?php the_permalink(); ?>">
 						<a href="<?php the_permalink(); ?>" class="Category__item__thumbnail">
-							<img src="https://i.ytimg.com/vi/<?php echo esc_attr( get_post_meta( get_the_ID(), 'mb_videos_mb_videos_video_id', true ) ); ?>/hqdefault.jpg" alt="<?php _e( 'Videos', 'ms' ); ?>">
+							<img src="https://i.ytimg.com/vi/<?php echo esc_attr( get_post_meta( get_the_ID(), 'mb_videos_mb_videos_video_id', true ) ); ?>/hqdefault.jpg" alt="<?php _e( 'Videos', 'urlslab' ); ?>">
 						</a>
-							<h3 class="Category__item__title item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<h3 class="Category__item__title item-title" data-title><a href="<?php the_permalink(); ?>"><?= esc_html( wp_trim_words( get_the_title(), 7 ) ); ?></a></h3>
 							<div class="Category__item__excerpt item-excerpt">
-								<a href="<?php the_permalink(); ?>">
-									<?= esc_html( wp_trim_words( get_the_excerpt(), 16 ) ); ?>
+								<a href="<?php the_permalink(); ?>" data-excerpt>
+									<?= esc_html( wp_trim_words( do_shortcode( '[urlslab-video attribute="description" id="' . get_post_meta( get_the_ID(), 'mb_videos_mb_videos_shortcode_id', true ) . '" videoid="' . get_post_meta( get_the_ID(), 'mb_videos_mb_videos_video_id', true ) . '"]' ), 20 ) );  ?>
 								</a>
 							</div>
+
+							<div class="Category__item__duration"><?=  esc_html( duration_to_time( do_shortcode( '[urlslab-video attribute="duration" id="' . get_post_meta( get_the_ID(), 'mb_videos_mb_videos_shortcode_id', true ) . '" videoid="' . get_post_meta( get_the_ID(), 'mb_videos_mb_videos_video_id', true ) . '"]' ) ) ); ?> </div>
 					</li>
 
 					<?php
