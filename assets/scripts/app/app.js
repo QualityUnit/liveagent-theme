e9065f7274a79fba1d76131b9cffbc8151ca265d
Tomáš Hujčák <134269593+thujcak@users.noreply.github.com>
2023-12-18 14:11:16 +0100

    [1327] added new pinned trial button (#1773)
    * feat: added stickytrial btn to page and animate contactus btn
    
    * feat: changed event listener scroll to timeout

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -379,4 +379,43 @@
 			} );
 		} );
 	}
+	/* Trial sticky button at the bottom of the page */
+	if ( query( '.trial__sticky__button' ) !== null ) {
+		window.addEventListener( 'load', function() {
+			if ( window.matchMedia( '(max-width: 1024px)' ).matches ) {
+				const trialStickyButtonClosedValue = sessionStorage.getItem( 'trialStickyButtonClosed' );
+				let isClosed = trialStickyButtonClosedValue === 'true';
+
+				function showTrialStickyButton() {
+					const trialStickyButton = document.querySelector( '.trial__sticky__button' );
+					const contactUs = document.querySelector( '.ContactUs' );
+
+					if ( trialStickyButton && contactUs && ! isClosed ) {
+						trialStickyButton.classList.add( 'active' );
+						contactUs.classList.add( 'active' );
+					}
+				}
+
+				function closeTrialStickyButton() {
+					const closeButton = document.querySelector( '.trial__sticky__button--close' );
+					const trialStickyButton = document.querySelector( '.trial__sticky__button' );
+					const contactUs = document.querySelector( '.ContactUs' );
+
+					if ( closeButton && trialStickyButton && contactUs ) {
+						closeButton.addEventListener( 'click', function() {
+							trialStickyButton.classList.remove( 'active' );
+							contactUs.classList.remove( 'active' );
+							sessionStorage.setItem( 'trialStickyButtonClosed', 'true' );
+							isClosed = true;
+						} );
+					}
+				}
+
+				setTimeout( showTrialStickyButton, 7000 );
+
+				closeTrialStickyButton();
+			}
+		} );
+	}
 } )();
+
