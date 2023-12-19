d7e890334958708f609f52a11e3b76a84c8b18a6
Jozef Remeň <jozef.remen@gmail.com>
2023-12-19 15:18:11 +0100

    fix: function not defined

--- functions.php
+++ functions.php
@@ -42,6 +42,7 @@ $theme_includes = array(
 	'functions/widgets.php', // Import Widgets/Plugins
 	'functions/remove-base-classes.php', // Remove base classes
 	'functions/get-cta-button-data.php', // Get data from metaboxes for cta button in the compact header
+	'functions/header-banners.php', // Shows banners on selected pages
 );
 
 foreach ( $theme_includes as $file ) {
