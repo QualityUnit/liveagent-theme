a2174abd1bcaa14335a443b3c571170934e82042
Jozef Remeň <jozef.remen@gmail.com>
2023-12-21 13:47:37 +0100

    fix: replacing scroll event with intersection observer

--- assets/scripts/custom/AnnouncementBar.js
+++ assets/scripts/custom/AnnouncementBar.js
@@ -1,91 +1,85 @@
 
+const announcementBarSliders = document.querySelectorAll( '.Announcement__bars__slider' );
 const announcementBars = document.querySelectorAll( '.Announcement__bars__slider .Announcement__bar' );
 const appContainer = document.getElementById( 'app' );
+const announcementClosed = sessionStorage.getItem( 'announcementClosed' );
 
-function showAnnouncementBar() {
-	appContainer.classList.add( 'announcement--active' );
+if ( announcementClosed === 'true' ) {
+	announcementBarSliders.forEach( ( slider ) => slider.remove() );
 }
 
-function hideAnnouncementBar() {
-	appContainer.classList.remove( 'announcement--active' );
-	appContainer.classList.add( 'announcement--hidden' );
-	appContainer.classList.add( 'announcement--closed' );
-	sessionStorage.setItem( 'announcementClosed', 'true' );
-}
+if ( announcementClosed !== 'true' ) {
+	appContainer.insertAdjacentHTML( 'afterbegin', '<div class="observerPoint pos-absolute w-100" style="top: 800px;"></div>' );
 
-function initializeAnnouncementBar() {
-	const scrollHeight = window.scrollY;
-	const closeButton = document.querySelector( '.Announcement__bar__close' );
-
-	if ( scrollHeight >= 800 ) {
-		appContainer.classList.remove( 'announcement--hidden' );
+	function showAnnouncementBar() {
 		appContainer.classList.add( 'announcement--active' );
 	}
 
-	closeButton.addEventListener( 'click', function() {
-		hideAnnouncementBar();
-	} );
-}
-
-function toggleAnnouncementBar() {
-	const scrollHeight = window.scrollY;
-
-	const announcementClosed = sessionStorage.getItem( 'announcementClosed' );
-	if ( announcementClosed === 'true' ) {
-		appContainer.classList.add( 'announcement--hidden' );
-	} else if ( scrollHeight >= 800 ) {
+	function hideAnnouncementBar() {
 		appContainer.classList.remove( 'announcement--active' );
 		appContainer.classList.add( 'announcement--hidden' );
-	} else if ( ! appContainer.classList.contains( 'announcement--closed' ) ) {
-		if ( appContainer.classList.contains( 'announcement--hidden' ) ) {
-			appContainer.classList.add( 'announcement--active' );
+		sessionStorage.setItem( 'announcementClosed', 'true' );
+	}
+
+	function initializeAnnouncementBar() {
+		const scrollHeight = window.scrollY;
+		const closeButton = document.querySelector( '.Announcement__bar__close' );
+
+		if ( scrollHeight >= 800 ) {
 			appContainer.classList.remove( 'announcement--hidden' );
+			appContainer.classList.add( 'announcement--active' );
 		}
+
+		closeButton.addEventListener( 'click', function() {
+			hideAnnouncementBar();
+		} );
 	}
-}
 
-document.addEventListener( 'DOMContentLoaded', function() {
-	const announcementClosed = sessionStorage.getItem( 'announcementClosed' );
-	if ( ! announcementClosed ) {
-		showAnnouncementBar();
-	}
-	initializeAnnouncementBar();
-} );
-
-const AnnouncementBarObserver = new IntersectionObserver(
-	( [ entry ] ) => {
-		console.log( entry.boundingClientRect );
-		if ( entry.isIntersecting ) {
-			console.log( 'picaaa' );
-		}
-		if ( entry.boundingClientRect.top > 800 ) {
-			console.log( 'gule' );
-		}
-	},
-	{
-		root: null,
-		rootMargin: `0px 0px -100% 0px`,
-	}
-);
-
-// AnnouncementBarObserver.observe( document.querySelector( '.AppContainer > *:first-child' ) );
-
-// window.addEventListener( 'scroll', toggleAnnouncementBar );
-
-if ( announcementBars.length > 1 ) {
-	let counter = 0;
-
-	setInterval( () => {
-		counter = counter + 1;
-
-		if ( counter === announcementBars.length ) {
-			announcementBars.item( 0 ).classList.add( 'active' );
-			announcementBars.item( counter - 1 ).classList.remove( 'active' );
-			counter = 0;
+	function toggleAnnouncementBar( hide ) {
+		if ( hide ) {
+			appContainer.classList.remove( 'announcement--active' );
+			appContainer.classList.add( 'announcement--hidden' );
 			return false;
 		}
+		appContainer.classList.add( 'announcement--active' );
+		appContainer.classList.remove( 'announcement--hidden' );
+	}
 
-		announcementBars.item( counter ).classList.add( 'active' );
-		announcementBars.item( counter - 1 ).classList.remove( 'active' );
-	}, 8000 );
+	document.addEventListener( 'DOMContentLoaded', function() {
+		if ( ! announcementClosed ) {
+			showAnnouncementBar();
+		}
+		initializeAnnouncementBar();
+	} );
+
+	const AnnouncementBarObserver = new IntersectionObserver(
+		( [ entry ] ) => {
+			if ( entry.isIntersecting ) {
+				toggleAnnouncementBar( false );
+			}
+			if ( ! entry.isIntersecting ) {
+				toggleAnnouncementBar( true );
+			}
+		}
+	);
+
+	AnnouncementBarObserver.observe( document.querySelector( '.observerPoint' ) );
+
+	if ( announcementBars.length > 1 ) {
+		let counter = 0;
+
+		setInterval( () => {
+			counter = counter + 1;
+
+			if ( counter === announcementBars.length ) {
+				announcementBars.item( 0 ).classList.add( 'active' );
+				announcementBars.item( counter - 1 ).classList.remove( 'active' );
+				counter = 0;
+				return false;
+			}
+
+			announcementBars.item( counter ).classList.add( 'active' );
+			announcementBars.item( counter - 1 ).classList.remove( 'active' );
+		}, 8000 );
+	}
 }
