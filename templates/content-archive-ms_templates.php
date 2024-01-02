a60af7dd5c3dfc90ac65eb2def5fefed767518d1
Jozef Remeň <jozef.remen@gmail.com>
2024-01-02 13:06:18 +0100

    feat: templates categories unification

--- templates/content-archive-ms_templates.php
+++ templates/content-archive-ms_templates.php
@@ -1,5 +1,8 @@
 <?php // @codingStandardsIgnoreLine
 set_source( 'templates', 'pages/Category', 'css' );
+set_source( 'templates', 'pages/CategoryImages', 'css' );
+set_source( 'templates', 'pages/CategoryItemBgs', 'css' );
+set_source( 'templates', 'pages/CategoryColorScheme', 'css' );
 set_source( 'templates', 'filter', 'js' );
 $categories        = array_unique( get_categories( array( 'taxonomy' => 'ms_templates_categories' ) ), SORT_REGULAR );
 $page_header_title = __( 'Customer Service Templates', 'ms' );
@@ -52,77 +55,108 @@ $page_header_args = array(
 				<?php
 				while ( have_posts() ) :
 					the_post();
+					$category       = '';
+					$category_title = '';
+					$cat_item       = '';
 
-					$category = '';
+					$categories   = get_the_terms( 0, 'ms_templates_categories' );
+					$current_lang = apply_filters( 'wpml_current_language', null );
+					do_action( 'wpml_switch_language', 'en' );
+					$categories_en = get_the_terms( 0, 'ms_templates_categories' );
+					if ( ! empty( $categories_en ) ) {
+						$category_en = array_shift( $categories_en )->slug;
+					}
 
-					$categories = get_the_terms( 0, 'ms_templates_categories' );
+					do_action( 'wpml_switch_language', $current_lang );
 
 					if ( ! empty( $categories ) ) {
 						foreach ( $categories as $category_item ) {
-							$category_item = array_shift( $categories );
-							$category     .= $category_item->slug;
-							$category     .= ' ';
+							$category_item  = array_shift( $categories );
+							$category      .= $category_item->slug;
+							$category      .= ' ';
+							$cat_item       = $category_item;
+							$category_title = $category_item->name;
 						}
 					}
 
 					$category = substr( $category, 0, -1 );
-					?>
 
-					<li class="Category__item
-					<?php
-					if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) {
-						echo 'pillar'; }
-					?>
-					" data-category="<?= esc_attr( $category ); ?>" data-href="<?php the_permalink(); ?>">
-						<a href="<?php the_permalink(); ?>" class="Category__item__thumbnail">
-						<?php if ( has_post_thumbnail() ) { ?>
-							<?php if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) { ?>
-								<span class="Category__item__thumbnail__image">
-								<?php
-							}
-								the_post_thumbnail( 'archive_thumbnail' );
-							if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) {
-								?>
-								</span>
-							<?php } ?>
-							<?php } else { ?>
-								<?php if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) { ?>
-									<span href="<?php the_permalink(); ?>" class="Category__item__thumbnail__image">
-										<?php } ?>
-									<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon-book.svg" alt="<?php _e( 'Templates', 'ms' ); ?>">
-									<?php if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) { ?>
-								</span>
-								<?php } ?>
-							<?php } ?>
-						</a>
-						<?php
-						if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) {
-							?>
-								<div class="Category__item__wrap">
-							<?php
-						}
+					// Custom link for items
+					$custom_link = get_post_meta( get_the_ID(), 'mb_custom_url', true );
+					$item_url    = $custom_link ? $custom_link : get_the_permalink();
+
+					// Element classes.
+					$pillar_value = ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) ?? '' );
+					$pillar_class = '';
+					if ( 'on' === $pillar_value ) {
+							$pillar_class = 'pillar';
+					}
+					$category_item_classes      = 'Category__item redesign Category__item--templates ' . $pillar_class . ' ' . esc_attr( $category_en );
+					$category_card_item_classes = 'Category__item redesign Category__item--templates ' . esc_attr( $category ) . ' ' . esc_attr( $category_en );
+
+					// Element attributes.
+					$category_item_attributes = array(
+						'data-category' => esc_attr( $category ),
+						'data-href'     => esc_url( $item_url ),
+					);
+
+					if ( 'on' === $pillar_value ) :
 						?>
-							<h3 class="Category__item__title item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-							<div class="Category__item__excerpt item-excerpt">
-								<a href="<?php the_permalink(); ?>">
-									<?= esc_html( wp_trim_words( get_the_excerpt(), 16 ) ); ?>
-									<?php if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) { ?>
-										<span><?php _e( 'Read More', 'ms' ); ?></span>
-									<?php } ?>
-								</a>
-							</div>
-							<?php
-							if ( get_post_meta( get_the_ID(), 'mb_templates_mb_templates_pillar', true ) === 'on' ) {
-								?>
+							<li class="<?= esc_attr( $category_item_classes ); ?>"
+										<?php
+										foreach ( $category_item_attributes as $name => $value ) {
+											echo esc_html( $name ) . '="' . esc_attr( $value ) . '" ';
+										}
+										?>
+							>
+							<a href="<?= esc_url( $item_url ) ?>" class="Category__item__thumbnail">
+								<span class="Category__item__thumbnail__image"></span>
+							</a>
+							<div class="Category__item__wrap">
+								<h2 class="Category__item__title item-title"><a href="<?= esc_url( $item_url ) ?>"><?php the_title(); ?></a></h2>
+								<div class="Category__item__excerpt item-excerpt">
+									<a href="<?= esc_url( $item_url ) ?>">
+						<?= esc_html( wp_trim_words( get_the_excerpt(), 14 ) ); ?>
+									</a>
 								</div>
-								<?php
-							}
-							?>
-					</li>
+								<a class="Category__item__cta" href="<?= esc_url( $item_url ) ?>"><?php esc_html_e( 'Learn more', 'ms' ); ?></a>
+							</div>
+							</li>
+						<?php else : ?>
+							<li class="<?= esc_attr( $category_card_item_classes ); ?>"
+													<?php
+													foreach ( $category_item_attributes as $name => $value ) {
+														echo esc_html( $name ) . '="' . esc_attr( $value ) . '" ';
+													}
+													?>
+								>
+								<div class="Category__item__wrap">
+									<div class="Category__item__header">
+										<div class="Category__item__header__image">
+													<?php
+													if ( has_post_thumbnail() ) {
+														the_post_thumbnail( 'archive_thumbnail' );
+													} else {
+														?>
+					<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon-custom-post_type.svg" alt="<?php esc_attr_e( 'Templates', 'ms' ); ?>">
 
-					<?php
-						$category = '';
-					?>
+													<?php	} ?>
+			</div>
+										<div class="Category__item__header__label">
+											<span class="Category__item__header__label__text"><?= esc_html( $category_title ); ?></span>
+										</div>
+									</div>
+									<div class="Category__item__content">
+										<h3 class="Category__item__content__title item-title"><a href="<?= esc_url( $item_url ) ?>"><?php the_title(); ?></a></h3>
+										<div class="Category__item__content__excerpt item-excerpt">
+											<a href="<?= esc_url( $item_url ) ?>">
+													<?= esc_html( wp_trim_words( get_the_excerpt(), 14 ) ); ?>
+											</a>
+										</div>
+									</div>
+								</div>
+							</li>
+		<?php endif; ?>
 
 				<?php endwhile; ?>
 			</ul>
