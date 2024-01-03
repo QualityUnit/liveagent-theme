7da3c6f9d3624a1f3256adccce0210d2aa766cf4
Jozef Remeň <jozef.remen@gmail.com>
2024-01-03 11:21:53 +0100

    feat: header images for templates single page

--- templates/content-single-ms_templates.php
+++ templates/content-single-ms_templates.php
@@ -10,20 +10,21 @@
 	if ( has_post_thumbnail() ) {
 		$page_header_logo['src'] = get_the_post_thumbnail_url( $post, 'logo_thumbnail' );
 	}
-	$page_header_args = array(
-		'image' => array(
-			'src' => get_template_directory_uri() . '/assets/images/compact_header_templates.png?ver=' . THEME_VERSION,
+	$page_header_image = 'templates-category_' . $header_category . '.jpg';
+	$page_header_args  = array(
+		'image'      => array(
+			'src' => get_template_directory_uri() . '/assets/images/' . $page_header_image . '?ver=' . THEME_VERSION,
 			'alt' => get_the_title(),
 		),
-		'logo'  => $page_header_logo,
-		'title' => get_the_title(),
-		'text'  => do_shortcode( '[urlslab-generator id="6"]' ),
-		'toc'   => true,
+		'logo'       => $page_header_logo,
+		'title'      => get_the_title(),
+		'text'       => do_shortcode( '[urlslab-generator id="6"]' ),
+		'toc'        => true,
 		'cta_button' => get_cta_button_data(),
 	);
-	$current_id       = apply_filters( 'wpml_object_id', $post->ID, 'ms_templates' );
-	$categories       = get_the_terms( $current_id, 'ms_templates_categories' );
-	$categories_url   = get_post_type_archive_link( 'ms_templates' );
+	$current_id        = apply_filters( 'wpml_object_id', $post->ID, 'ms_templates' );
+	$categories        = get_the_terms( $current_id, 'ms_templates_categories' );
+	$categories_url    = get_post_type_archive_link( 'ms_templates' );
 	if ( $categories && $categories_url ) {
 		$new_tags = array(
 			'title' => __( 'Categories', 'ms' ),
