75240a5e191db621ea62220a1449a12f3a6174a9
Tomáš Hujčák <134269593+thujcak@users.noreply.github.com>
2024-01-09 11:34:42 +0100

    feat: changed text in trial forms and fixed tooltips (#1844)

--- assets/scripts/app/tooltips.js
+++ assets/scripts/app/tooltips.js
@@ -1,69 +1,72 @@
 /* global Popper */
 /* Tooltips */
-tooltips( [ 'Tooltip', 'ComparePlans__tooltip' ] );
-function tooltips( classList = [] ) {
-	classList.forEach( ( className ) => {
-		if ( document.querySelectorAll( '.' + className ).length > 0 && typeof Popper !== 'undefined' ) {
-			document.querySelectorAll( '.' + className ).forEach( ( trigger ) => {
-				const classText = className + '__text';
-				const tooltip = trigger.querySelector( '.' + classText );
-				if ( tooltip ) {
-					let defaultPlacement = 'bottom';
-					const arrow = document.createElement( 'span' );
-					arrow.setAttribute( 'data-popper-arrow', '' );
-					tooltip.appendChild( arrow );
+document.addEventListener( 'DOMContentLoaded', () => {
+	tooltips( [ 'Tooltip', 'ComparePlans__tooltip' ] );
 
-					if ( tooltip.classList.contains( classText + '--top' ) ) {
-						defaultPlacement = 'top';
-					}
+	function tooltips( classList = [] ) {
+		classList.forEach( ( className ) => {
+			if ( document.querySelectorAll( '.' + className ).length > 0 && typeof Popper !== 'undefined' ) {
+				document.querySelectorAll( '.' + className ).forEach( ( trigger ) => {
+					const classText = className + '__text';
+					const tooltip = trigger.querySelector( '.' + classText );
+					if ( tooltip ) {
+						let defaultPlacement = 'bottom';
+						const arrow = document.createElement( 'span' );
+						arrow.setAttribute( 'data-popper-arrow', '' );
+						tooltip.appendChild( arrow );
 
-					const popperInstance = Popper.createPopper( trigger, tooltip, {
-						placement: defaultPlacement,
-						modifiers: [
-							{
-								name: 'offset',
-								options: {
-									offset: [ 0, 6 ],
+						if ( tooltip.classList.contains( classText + '--top' ) ) {
+							defaultPlacement = 'top';
+						}
+
+						const popperInstance = Popper.createPopper( trigger, tooltip, {
+							placement: defaultPlacement,
+							modifiers: [
+								{
+									name: 'offset',
+									options: {
+										offset: [ 0, 6 ],
+									},
 								},
-							},
-						],
-					} );
-
-					function show() {
-						tooltip.setAttribute( 'data-show', '' );
-						popperInstance.setOptions( ( options ) => ( {
-							...options,
-							modifiers: [
-								...options.modifiers,
-								{ name: 'eventListeners', enabled: true },
 							],
-						} ) );
-						popperInstance.update();
+						} );
+
+						function show() {
+							tooltip.setAttribute( 'data-show', '' );
+							popperInstance.setOptions( ( options ) => ( {
+								...options,
+								modifiers: [
+									...options.modifiers,
+									{ name: 'eventListeners', enabled: true },
+								],
+							} ) );
+							popperInstance.update();
+						}
+
+						function hide() {
+							tooltip.removeAttribute( 'data-show' );
+							popperInstance.setOptions( ( options ) => ( {
+								...options,
+								modifiers: [
+									...options.modifiers,
+									{ name: 'eventListeners', enabled: false },
+								],
+							} ) );
+						}
+
+						const showEvents = [ 'mouseenter', 'focus' ];
+						const hideEvents = [ 'mouseleave', 'blur' ];
+
+						showEvents.forEach( ( event ) => {
+							trigger.addEventListener( event, show );
+						} );
+
+						hideEvents.forEach( ( event ) => {
+							trigger.addEventListener( event, hide );
+						} );
 					}
-
-					function hide() {
-						tooltip.removeAttribute( 'data-show' );
-						popperInstance.setOptions( ( options ) => ( {
-							...options,
-							modifiers: [
-								...options.modifiers,
-								{ name: 'eventListeners', enabled: false },
-							],
-						} ) );
-					}
-
-					const showEvents = [ 'mouseenter', 'focus' ];
-					const hideEvents = [ 'mouseleave', 'blur' ];
-
-					showEvents.forEach( ( event ) => {
-						trigger.addEventListener( event, show );
-					} );
-
-					hideEvents.forEach( ( event ) => {
-						trigger.addEventListener( event, hide );
-					} );
-				}
-			} );
-		}
-	} );
-}
+				} );
+			}
+		} );
+	}
+} );
