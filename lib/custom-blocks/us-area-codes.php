ac1d9a570ffd8e0eddb92c79de71037507dff3e8
Jozef Remeň <jozef.remen@gmail.com>
2024-01-10 13:55:46 +0100

    feat: search for usa country codes

--- lib/custom-blocks/us-area-codes.php
+++ lib/custom-blocks/us-area-codes.php
@@ -1,4 +1,5 @@
 <?php
+set_source( 'ms_ac_countries', 'filter', 'js' );
 
 // USA states and their corresponding phone area codes, lowercase names, and ANSI codes
 $us_area_codes = array(
@@ -255,17 +256,30 @@ $us_area_codes = array(
 );
 ?>
 	<h3><?php _e( 'List of USA area codes' ); ?></h3>
-	<div>
+	<div class="compact-header__search">
+			<div class="searchField">
+				<svg class="searchField__icon icon-search">
+					<use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#search' ) ?>"></use>
+				</svg>
+				<input type="search" class="search<?= esc_attr( $search_class ); ?>" placeholder="<?php _e( 'Search', 'ms' ); ?>" maxlength="50">
+				<span class="search-reset">
+		<svg class="search-reset__icon icon-close">
+			<use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#close' ) ?>"></use>
+		</svg>
+	</span>
+			</div>
+		</div>
+	<div class="list">
 		<?php
 		foreach ( $us_area_codes as $state => $values ) {
 			?>
-		<h4 class="state" itemprop="name"><a href="<?= $values['slug']; // @codingStandardsIgnoreLine ?>"><?= esc_html( $state ); ?></a></h4>
+		<h4 class="state" itemprop="name"><a class="item-title" href="../../usa/<?= $values['slug']; // @codingStandardsIgnoreLine ?>"><?= esc_html( $state ); ?></a></h4>
 		<ul class="area-codes flex flex-wrap">
 			<?php
 			foreach ( $values['area_codes'] as $area_code ) {
 				?>
 					<li class="area-code mr-xs">
-						<a href="<?= $area_code; // @codingStandardsIgnoreLine ?>"><?= esc_html( $area_code ); ?></a>
+						<a class="item-excerpt" href="../../codes/usa_<?= $area_code; // @codingStandardsIgnoreLine ?>"><?= esc_html( $area_code ); ?></a>
 					</li>
 					<?php
 			}
