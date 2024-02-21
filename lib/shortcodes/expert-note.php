cf5edaef55e4fa40ef413c234244974e0dd6b8f8
Jozef Remeň <jozef.remen@gmail.com>
2024-02-21 11:02:57 +0100

    fix: expert name, position and image as attributes with defaults

--- lib/shortcodes/expert-note.php
+++ lib/shortcodes/expert-note.php
@@ -1,9 +1,18 @@
 <?php
 
-function ms_expert_note() {
+function ms_expert_note( $atts ) {
 		$shortcode_text = do_shortcode( '[urlslab-generator id="1"]' );
+		$atts           = shortcode_atts(
+			array(
+				'photo'    => get_template_directory_uri() . '/assets/images/andrej_saxon_new.png?ver=' . THEME_VERSION,
+				'name'     => 'Andrej Saxon',
+				'position' => 'Sales manager',
+			),
+			$atts,
+			'expert-note'
+		);
 		ob_start();
-	if ( ! empty( $shortcode_text ) ) {
+	if ( empty( $shortcode_text ) ) {
 		?>
 
 	<div class="ExpertNote" itemprop="comment" itemscope itemtype="https://schema.org/Comment">
@@ -13,10 +22,10 @@ function ms_expert_note() {
 		<div class="ExpertNote__agent" itemprop="author" itemscope itemtype="https://schema.org/Person">
 			<div class="ExpertNote__agent--inn">
 				<div class="ExpertNote__agent--info">
-					<span class="ExpertNote__agent--name" itemprop="name"><?php _e( 'Andrej Saxon', 'ms' ); ?></span>
-					<strong class="ExpertNote__agent--position" itemprop="jobTitle"><?php _e( 'Sales manager', 'ms' ); ?></strong>
+					<span class="ExpertNote__agent--name" itemprop="name"><?= esc_html( __( $atts['name'], 'ms' ) ); ?></span>
+					<strong class="ExpertNote__agent--position" itemprop="jobTitle"><?= esc_html( __( $atts['position'], 'ms' ) ); ?></strong>
 				</div>
-				<img class="ExpertNote__agent--image" itemprop="image" src="<?= esc_url( get_template_directory_uri() . '/assets/images/andrej_saxon_new.png?ver=' . THEME_VERSION ); ?>" alt="<?php _e( 'Andrej Saxon', 'ms' ); ?>" >
+				<img class="ExpertNote__agent--image" itemprop="image" src="<?= esc_url( $atts['photo'] ); ?>" alt="<?php _e( 'Andrej Saxon', 'ms' ); ?>" >
 			</div>
 		</div>
 	</div>
