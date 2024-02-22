ddd3645044c46ee8e1db5428e5b659ce11102529
thujcak <thujcak@qualityunit.com>
2024-02-22 12:10:58 +0100

    feat: changed h1 title to h2

--- lib/shortcodes/signup-form-free.php
+++ lib/shortcodes/signup-form-free.php
@@ -12,7 +12,7 @@ function ms_signup_form_free() {
 	?>
 
 	<div class="Signup__form ">
-		<h1 class="Signup__form__title h3"><?php _e( 'Create FREE account', 'ms' ); ?></h1>
+		<h2 class="Signup__form__title h3"><?php _e( 'Create FREE account', 'ms' ); ?></h2>
 		<p class="Signup__form__subtitle"><?php _e( 'The FREE account is free forever, but it comes with limited features.', 'ms' ); ?></p>
 
 		<div data-id="signup" data-type='free'>
@@ -76,7 +76,7 @@ function ms_signup_form_free() {
 					<div class="WorkingPanel" style="display: none;">
 						<img class="gear-wheels" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/gear-wheels.gif' ); ?>" alt="gear wheels">
 					</div>
-					<span><?php _e( 'Create FREE account', 'ms' ); ?></span>
+					<sapn><?php _e( 'Create FREE account', 'ms' ); ?></sapn>
 				</div>
 
 				<div class="Signup__form__terms">
