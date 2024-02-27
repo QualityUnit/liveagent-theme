cb0f285da4f9947ed8ebf347d134d462b56703aa
Jozef Remen <jozef.remen@gmail.com>
2024-02-27 07:08:44 +0100

    feat:  new awards in trial, pagespeed fixes to login and header formatting (#1972)

--- template-trial-redesign.php
+++ template-trial-redesign.php
@@ -14,20 +14,21 @@
 		<div class="Trial__sidebar urlslab-min-width-1024">
 			<div class="Trial__sidebar__inner">
 				<div class="Trial__sidebar__content checklist">
-					<h3 class="Trial__sidebar__title">Handle all support channels in one solution</h3>
+					<h3 class="Trial__sidebar__title"><?php _e( 'Handle all support channels in one solution', 'ms' ); ?></h3>
 					<ul>
-						<li>Spend 85% less time on issues</li>
-						<li>Save 95% more money on support</li>
-						<li>Gain 56% more returning customers</li>
-						<li>Get 73% higher satisfaction rates</li>
+						<li><?php _e( 'Spend 85% less time on issues', 'ms' ); ?></li>
+						<li><?php _e( 'Save 95% more money on support', 'ms' ); ?></li>
+						<li><?php _e( 'Gain 56% more returning customers', 'ms' ); ?></li>
+						<li><?php _e( 'Get 73% higher satisfaction rates', 'ms' ); ?></li>
 					</ul>
 				</div>
 			</div>
 		</div>
 		<div class="Trial__main">
-			<a href="<?= esc_url( home_url( '/', 'relative' ) ); ?>" class="Trial__awards">
-				<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/trial_badges.svg" alt="<?php _e( 'Awards', 'ms' ); ?>" class="urlslab-skip-lazy">
-			</a>
+			<div class="heroBanner__awards">
+				<?php echo do_shortcode( '[hero-banner-awards]' ); ?>
+			</div>
+			
 			<div class="Trial__main__inner">
 				<h1 class="Trial__main__title"><?php _e( 'Start your <span class="highlight-gradient">free trial</span> today', 'ms' ); ?></h1>
 				<p class="Trial__main__text"><?php _e( 'Experience working with LiveAgent for free with our 14 or 30 days free trial. You can enjoy every feature available in the Large plan for free without any strings attached, and select your new LiveAgent plan at the end of your trial.', 'ms' ); ?></p>
