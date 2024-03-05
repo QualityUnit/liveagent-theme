d9e4636c70ec60f75ffa780b1b1c8224435d4d20
Jozef Remeň <jozef.remen@gmail.com>
2024-03-05 14:37:00 +0100

    fix: awards in trial

--- lib/shortcodes/hero-banner-awards.php
+++ lib/shortcodes/hero-banner-awards.php
@@ -1,24 +1,36 @@
 <?php
 
-function ms_hero_banner_awards() {
+function ms_hero_banner_awards( $atts ) {
+	$atts = shortcode_atts(
+		array(
+			'title' => '',
+			'bg'    => '',
+		),
+		$atts,
+		'hero-banner-awards'
+	);
+	set_source( false, 'shortcodes/HeroBannerAwards' );
 	ob_start();
 	?>
 	<div class="heroBanner__awards__wrapper">
-		<div class="heroBanner__awards__bg" style="background: url(<?= esc_url( get_template_directory_uri() . '/assets/images/hero-banner-awards.png?ver=' . THEME_VERSION ); ?>) center top no-repeat; background-size: contain"></div>
+		<div class="heroBanner__awards__bg" style="background: url(<?= esc_url( get_template_directory_uri() . '/assets/images/' . ( ! empty( $atts['bg'] ) ? $atts['bg'] : 'hero-banner-awards.png' ) . '?ver=' . THEME_VERSION ); ?>) center top no-repeat; background-size: contain"></div>
 	
+		<?php
+		if ( $atts['title'] !== 'false' ) {
+			?>
 		<img class="heroBanner__awards__title" src="<?= esc_url( get_template_directory_uri() . '/assets/images/hero-banner-awards__title.png?ver=' . THEME_VERSION ); ?>" alt="Awards" />
-	<?php
-	foreach ( get_awards( 3 ) as $award_id ) {
-		?>
+			<?php
+		}
+		foreach ( get_awards( 3 ) as $award_id ) {
+			?>
 
 	<div class="heroBanner__awards__img" style="background: url(<?= esc_attr( get_the_post_thumbnail_url( $award_id, 'box_archive_thumbnail' ) ); ?>) center center no-repeat"></div>
 
-		<?php
-	}
-	?>
+			<?php
+		}
+		?>
 	</div>
 	<?php
-	set_custom_source( 'shortcodes/HeroBannerAwards', 'css', false, false );
 	return ob_get_clean();
 }
 add_shortcode( 'hero-banner-awards', 'ms_hero_banner_awards' );
