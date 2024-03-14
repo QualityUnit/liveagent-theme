4c7536e9ec1c27c8278c024f20047b58d2da19f3
Ivan Hudak <ivan.hdk@gmail.com>
2024-03-14 12:56:38 +0100

    fix: removed good hands section from thank-you and redeem-code

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <?php
 wp_enqueue_style( 'footer', get_template_directory_uri() . '/assets/dist/layouts/Footer' . isrtl() . wpenv() . '.css', false, THEME_VERSION );
 
-if ( empty( preg_grep( '/^(login|trial|free-account|demo|request-for-proposal)$/', get_body_class() ) ) ) {
+if ( empty( preg_grep( '/^(login|trial|thank-you|redeem-code|free-account|demo|request-for-proposal)$/', get_body_class() ) ) ) {
 	echo do_shortcode( '[good-hands-redesign]' );
 	$footer_bg = get_template_directory_uri() . '/assets/images/footer-bg.svg';
 	?>
