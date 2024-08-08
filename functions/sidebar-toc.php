d247424a8e2666222ce735d5fe66f82790f754c5
Jozef Remeň <jozef.remen@gmail.com>
2024-08-08 08:13:40 +0200

    fix: no h3 in compact header

--- functions/sidebar-toc.php
+++ functions/sidebar-toc.php
@@ -1,7 +1,7 @@
 <?php
 
 
-//Add ID to every H2 element in post/page that has no ID. Generated from sanitized text content of element
+//Add ID to every H2 and H3 element in post/page that has no ID. Generated from sanitized text content of element
 function add_id_to_h2( $html ) {
 	$html = preg_replace_callback(
 		'/\<h2( id=".+?")?\>(.+?)\<\/h2\>/',
@@ -23,6 +23,14 @@ function add_id_to_h2( $html ) {
 		'/\<h3( id=".+?")?\>(.+)\<\/h3\>/',
 		function ( $m ) {
 			return '<h3 id="h-' . preg_replace( '/[%\/\:]/i', '', sanitize_title( $m[2] ) ) . '">' . $m[2] . '</h3>';
+		},
+		$html
+	);
+
+	$html = preg_replace_callback(
+		'/\<h3(((?!id).).+?)\>(.+?)\<\/h3\>/',
+		function ( $m ) {
+			return '<h3 ' . $m[1] . ' id="h-' . preg_replace( '/[%\/]/i', '', sanitize_title( $m[3] ) ) . '">' . $m[3] . '</h3>';
 		},
 		$html
 	);
