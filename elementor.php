8ad74ce62817888e631c9e2fb569a19b7c64dead
Tomáš Hujčák <134269593+thujcak@users.noreply.github.com>
2024-08-14 15:17:44 +0200

    feat: added conditions for elementor template and page template (#2420)

--- elementor.php
+++ elementor.php
@@ -11,13 +11,18 @@ while ( have_posts() ) :
 
 	<?php
 	if ( ! is_page( array( 'sitemap' ) ) && ! is_front_page() ) {
-		?>
-		<div class="SimilarSources">
-			<div class="wrapper">
-				<div class="SimilarSources__title h3"><?php _e( 'Related Articles to ', 'ms' ); ?><?php the_title(); ?></div>
-				<?php echo do_shortcode( '[urlslab-related-resources related-count="6" show-image="true" show-summary="true"]' ); ?>
+
+		$related_resources = do_shortcode( '[urlslab-related-resources url="' . get_the_permalink() . '" related-count="4" show-image="true" show-summary="true"]' );
+
+		if ( ! empty( $related_resources ) ) {
+			?>
+			<div class="SimilarSources">
+				<div class="wrapper">
+					<div class="SimilarSources__title h3"><?php _e( 'Related Articles to ', 'ms' ); ?><?php the_title(); ?></div>
+					<?= wp_kses_post( $related_resources ); ?>
+				</div>
 			</div>
-		</div>
+		<?php } ?>
 	<?php } ?>
 
 <?php endwhile; ?>
--- templates/content-page.php
+++ templates/content-page.php
@@ -12,15 +12,19 @@
 
 				<?php
 				if ( ! is_page( array( 'sitemap' ) ) ) {
-					?>
-				<div class="SimilarSources SimilarSources--blog">
-					<div class="wrapper">
-						<div class="SimilarSources__title h4"><?php _e( 'Related Articles', 'ms' ); ?></div>
 
-						<?php echo do_shortcode( '[urlslab-related-resources related-count="4" show-image="true" show-summary="true"]' ); ?>
+					$related_resources = do_shortcode( '[urlslab-related-resources url="' . get_the_permalink() . '" related-count="4" show-image="true" show-summary="true"]' );
+
+					if ( ! empty( $related_resources ) ) {
+						?>
+					<div class="SimilarSources SimilarSources--blog">
+						<div class="wrapper">
+							<div class="SimilarSources__title h4"><?php _e( 'Related Articles', 'ms' ); ?></div>
+								<?= wp_kses_post( $related_resources ); ?>
+						</div>
 					</div>
-				</div>
 				<?php } ?>
+			<?php } ?>
 			</div>
 
 		<?php endwhile; ?>
