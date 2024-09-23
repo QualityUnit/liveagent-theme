208bc0060563e182c84a196eb1dcd0e72d8c7997
Martin Stepanek <martin@stepanek.sk>
2024-09-23 09:08:04 +0200

    feat: disable searchaction schema

--- lib/cleaner/yoast.php
+++ lib/cleaner/yoast.php
@@ -5,3 +5,11 @@
   */
 
 add_filter( 'wpseo_debug_markers', '__return_false' );
+
+
+/**
+  * Disable SearchAction Schema.org as we don't have search
+	* https://developer.yoast.com/features/schema/pieces/searchaction/#disable-searchaction-output
+  */
+
+add_filter( 'disable_wpseo_json_ld_search', '__return_true' );
