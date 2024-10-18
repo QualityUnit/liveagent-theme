db8fd843f12b5275ef7b9cf08436c04e26b0ae29
Martin Stepanek <martin@stepanek.sk>
2024-10-18 10:51:37 +0200

    fix: text above related articles

--- templates/content-single-ms_glossary.php
+++ templates/content-single-ms_glossary.php
@@ -52,7 +52,7 @@ $page_header_args = array(
 				</div>
 
 				<?php
-					if ( str_contains( home_url(), 'www' ) ) {
+				if ( str_contains( home_url(), 'www' ) ) {
 					?>
 
 				<div class="Post__ug7">
