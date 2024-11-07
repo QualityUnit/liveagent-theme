5022f6bbcba35b35399f29888c993fec73f30bc3
Jozef Remeň <jozef.remen@gmail.com>
2024-11-07 11:33:20 +0100

    fix: blocking js and css files

--- lib/cleaner/assets.php
+++ lib/cleaner/assets.php
@@ -95,11 +95,17 @@ add_action(
 			wp_deregister_style( 'elementor-pro' );
 			wp_deregister_style( 'elementor-pro-frontend' );
 			wp_deregister_style( 'swiper' );
+			wp_deregister_style( 'mediaelement' );
+			wp_deregister_style( 'wp-mediaelement' );
 
 			wp_register_style( 'elementor-frontend', get_template_directory_uri() . '/assets/dist/common/elementor-custom' . isrtl() . wpenv() . '.css', false, THEME_VERSION, 'screen' );
 			wp_enqueue_style( 'elementor-frontend' );
 
 			wp_deregister_script( 'wp-embed' );
+			wp_deregister_script( 'elementor-sticky' );
+			wp_deregister_script( 'elementor-pro-webpack-runtime' );
+			wp_deregister_script( 'wpml-xdomain-data' );
+			wp_deregister_script( 'wpml-xdomain-data-js-extra' );
 		}
 	},
 	1000
