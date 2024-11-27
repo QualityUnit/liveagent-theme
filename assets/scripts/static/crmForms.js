ae16fd4265f5e13db9e2cb0193b47732ba953b03
thujcak <thujcak@qualityunit.com>
2024-11-27 22:01:04 +0100

    fix: added a check if there are more than 3 characters in the input

--- assets/scripts/static/crmForms.js
+++ assets/scripts/static/crmForms.js
@@ -269,12 +269,21 @@ class CrmFormHandler {
 		const regex = this.regexPatterns[ key ];
 		const value = element.value.trim();
 
+		// Check if the field is empty
 		if ( value === '' ) {
 			fields[ key ].main.classList.add( 'Error' );
 			this.setError( key, localized.textEmpty );
 			return false;
 		}
 
+		// Check if the value is less than 3 characters
+		if ( key === 'domain' && value.length < 3 ) {
+			fields[ key ].main.classList.add( 'Error' );
+			this.setError( key, localized.textTooShort );
+			return false;
+		}
+
+		// Validate against regex pattern
 		if ( regex.test( value ) ) {
 			this.setError( key, localized.invalid[ key ] );
 			return false;
