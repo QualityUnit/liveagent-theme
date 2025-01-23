934831106e506d3323c5973725cab35b64f4100e
Oleksandr Bondaruk <obondaruk@qualityunit.com>
2025-01-23 12:38:42 +0200

    fix: changed adding class in svg (#2793)

--- lib/shortcodes/alternatives.php
+++ lib/shortcodes/alternatives.php
@@ -16,13 +16,14 @@ function ms_alternatives() {
 	?>
 
 	<ul class="Alternatives__list list">
-		<?php 
+		<?php
 		foreach ( $alternatives_posts as $alternative_id ) {
+			$excerpt = get_the_excerpt( $alternative_id );
+			$additional_class = ( strlen( $excerpt ) > 0 ) ? 'hasExcerpt' : '';
 			?>
 				<li class="Alternative">
 					<a class="Alternative__inn" href="<?= esc_url( get_the_permalink( $alternative_id ) ); ?>">
-						<svg class="Alternative__icon  
-						<?= ( strlen( get_the_excerpt( $alternative_id ) ) > 0 ) ? 'hasExcerpt' : ''; ?>"
+						<svg class="Alternative__icon <?= esc_attr( $additional_class ); ?>"
 						xmlns="http://www.w3.org/2000/svg" xml:space="preserve" fill-rule="evenodd"  viewBox="0 0 17 16"><path fill="url(#a)" d="M9.743 15.664c.02.002.04.003.061.003h5A.667.667 0 0 0 15.47 15V7a1.667 1.667 0 0 0-1.666-1.667H10.47V2A1.667 1.667 0 0 0 8.804.333h-6a1.67 1.67 0 0 0-1.179.488A1.67 1.67 0 0 0 1.137 2v13c0 .368.299.667.667.667h7.875a.685.685 0 0 0 .064-.003Zm-.606-1.331V2a.334.334 0 0 0-.333-.333h-6A.335.335 0 0 0 2.47 2v12.333h2.667V13a.667.667 0 0 1 1.333 0v1.333h2.667Zm1.333 0V6.667h3.334a.334.334 0 0 1 .333.333v7.333H10.47Z"/><path fill="url(#b)" fill-rule="nonzero" d="M3.369 13.496a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm2.5 7.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm2.5 10a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm0-2.5a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Z"/><path fill="url(#c)" fill-rule="nonzero" d="M8.658 5.854a.501.501 0 0 0-.709-.708.498.498 0 0 0 .002.706.5.5 0 0 0 .707.002Z"/><path fill="url(#d)" fill-rule="nonzero" d="M8.369 3.496a.499.499 0 0 1-.419-.849.5.5 0 1 1 .419.849Zm4.435 9.004a.5.5 0 1 0-.002.998.5.5 0 0 0 .002-.998Zm0-2.5a.5.5 0 1 0-.002.998.5.5 0 0 0 .002-.998Zm0-2.5a.5.5 0 1 0-.002.998.5.5 0 0 0 .002-.998Zm-2 5a.5.5 0 1 0-.002.998.5.5 0 0 0 .002-.998Zm0-2.5a.5.5 0 1 0-.002.998.5.5 0 0 0 .002-.998Zm0-2.5a.5.5 0 1 0-.002.998.5.5 0 0 0 .002-.998Z"/><defs><linearGradient id="a" x1="0" x2="1" y1="0" y2="0" gradientTransform="matrix(13 0 0 13 1.804 8)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffbd39"/><stop offset="1" stop-color="#fa9531"/><stop offset="1" stop-color="#fa9531"/></linearGradient><linearGradient id="b" x1="0" x2="1" y1="0" y2="0" gradientTransform="matrix(6 0 0 6 2.804 8)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffbd39"/><stop offset="1" stop-color="#fa9531"/><stop offset="1" stop-color="#fa9531"/></linearGradient><linearGradient id="c" x1="0" x2="1" y1="0" y2="0" gradientTransform="scale(.99686) rotate(-45 11.074 -6.692)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffbd39"/><stop offset="1" stop-color="#fa9531"/><stop offset="1" stop-color="#fa9531"/></linearGradient><linearGradient id="d" x1="0" x2="1" y1="0" y2="0" gradientTransform="translate(7.804 8) scale(5.49989)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffbd39"/><stop offset="1" stop-color="#fa9531"/><stop offset="1" stop-color="#fa9531"/></linearGradient></defs></svg>
 
 						<div class="Alternative__content">
