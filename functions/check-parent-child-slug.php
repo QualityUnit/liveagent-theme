786c44e5a6a39601b25f6b922763c8c1d038b58a
Tomáš Hujčák <134269593+thujcak@users.noreply.github.com>
2025-02-12 08:22:03 +0100

    fix: function (#2860)

--- functions/check-parent-child-slug.php
+++ functions/check-parent-child-slug.php
@@ -1,8 +1,12 @@
 <?php
 
 function check_parent_child_slug( array $parent_slugs ): bool {
-
 	global $post;
+
+
+	if ( ! isset( $post ) || ! is_object( $post ) ) {
+		return false;
+	}
 
 	$slug = $post->post_name;
 	$parent_id = $post->post_parent;
