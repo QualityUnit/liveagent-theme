055505fdb4d1f42643549bcf5493864bc49a0790
olebondaruk <obondaruk@qualityunit.com>
2025-04-17 12:30:44 +0300

    feat: changed success story shortcode and style for home page

--- lib/shortcodes/good-hands.php
+++ lib/shortcodes/good-hands.php
@@ -44,57 +44,6 @@ function ms_good_hands_redesign( $atts ) {
 							</script>
 					<?php } ?>
 				</div>
-
-				<ul class="GoodHands__logos flex">
-					<li class="GoodHands__logo">
-						<a href="<?php _e( '/awards/', 'ms' ); ?>" title="Capterra">
-							<img src="<?= esc_url( get_template_directory_uri() . '/assets/images/logo-capterra.svg?ver=' . THEME_VERSION ); ?>" alt="Capterra">
-						</a>
-						<div class="GoodHands__stars">
-							<?php
-								$logos = 1;
-							while ( $logos <= 5 ) {
-								?>
-									<svg class="GoodHands__star"><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#star-solid' ); ?>"></use></svg>
-								<?php
-								$logos++;
-							}
-							?>
-						</div>
-					</li>
-					<li class="GoodHands__logo">
-						<a href="<?php _e( '/awards/', 'ms' ); ?>" title="G2 Crowd">
-							<img src="<?= esc_url( get_template_directory_uri() . '/assets/images/logo_g2.svg?ver=' . THEME_VERSION ); ?>" alt="G2 Crowd">
-						</a>
-						<div class="GoodHands__stars">
-							<?php
-								$logos = 1;
-							while ( $logos <= 5 ) {
-								?>
-									<svg class="GoodHands__star"><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#star-solid' ); ?>"></use></svg>
-								<?php
-								$logos++;
-							}
-							?>
-						</div>
-					</li>
-					<li class="GoodHands__logo">
-						<a href="<?php _e( '/awards/', 'ms' ); ?>" title="GetApp">
-							<img src="<?= esc_url( get_template_directory_uri() . '/assets/images/logo_getapp.svg?ver=' . THEME_VERSION ); ?>" alt="GetApp">
-						</a>
-						<div class="GoodHands__stars">
-							<?php
-								$logos = 1;
-							while ( $logos <= 5 ) {
-								?>
-									<svg class="GoodHands__star"><use xlink:href="<?= esc_url( get_template_directory_uri() . '/assets/images/icons.svg?ver=' . THEME_VERSION . '#star-solid' ); ?>"></use></svg>
-								<?php
-								$logos++;
-							}
-							?>
-						</div>
-					</li>
-				</ul>
 			</div>
 
 			<div class="GoodHands__image">
