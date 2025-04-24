0c435415b38fc496a3b88508eadf53d35cc619d2
olebondaruk <obondaruk@qualityunit.com>
2025-04-24 15:47:58 +0300

    feat: created post type and template for new page "Migration"

---
+++ lib/post-types/migrations.php
@@ -0,0 +1,41 @@
+<?php
+
+add_action(
+	'init',
+	function () {
+		$labels  = array(
+			'name'           => _x( 'Migrations', 'Post Type General Name', 'ms' ),
+			'singular_name'  => _x( 'Migration', 'Post Type Singular Name', 'ms' ),
+			'menu_name'      => __( 'Migrations', 'ms' ),
+			'name_admin_bar' => __( 'Migration', 'ms' ),
+		);
+		$rewrite = array(
+			'slug'       => 'Migrations',
+			'with_front' => true,
+			'pages'      => true,
+			'feeds'      => false,
+		);
+		$args    = array(
+			'label'               => __( 'Migration', 'ms' ),
+			'labels'              => $labels,
+			'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author' ),
+			'hierarchical'        => true,
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'menu_position'       => 45,
+			'menu_icon'           => 'dashicons-migrate',
+			'show_in_admin_bar'   => true,
+			'show_in_nav_menus'   => true,
+			'can_export'          => true,
+			'has_archive'         => true,
+			'exclude_from_search' => false,
+			'publicly_queryable'  => true,
+			'rewrite'             => $rewrite,
+			'capability_type'     => 'post',
+			'show_in_rest'        => true,
+		);
+		register_post_type( 'ms_migrations', $args );
+	},
+	0
+);
---
+++ templates/content-ms_migrations.php
@@ -0,0 +1,58 @@
+<?php // @codingStandardsIgnoreLine
+$query_args = array(
+	'post_type'      => 'ms_migrations',
+	'posts_per_page' => - 1,
+	'fields'         => 'ids',
+);
+
+$migrations_posts = new WP_Query( $query_args );
+wp_reset_query();
+$page_header_title       = __( 'Migration', 'ms' );
+$page_header_description = __( 'Get to know all LiveAgent features, that are part of the complex multi-channel help desk software. Described in one place and in depth.', 'ms' );
+$page_header_args        = array(
+	'type'   => 'lvl-1',
+	'image'  => array(
+		'src' => get_template_directory_uri() . '/assets/images/compact_header_migrations.png?ver=' . THEME_VERSION,
+		'alt' => $page_header_title,
+	),
+	'title'  => $page_header_title,
+	'text'   => $page_header_description,
+	'search' => array( 'type' => 'academy' ),
+);
+?>
+
+<div class="Migrations">
+	<?php get_template_part( 'lib/custom-blocks/compact-header', null, $page_header_args ); ?>
+
+	<div class="wrapper Migrations__container">
+		<div class="Migrations__content">
+			<ul class="Migrations__content__items list">
+				<?php
+				while ( have_posts() === true ) :
+					the_post();
+
+					// Custom link for items
+					$custom_link = get_post_meta( get_the_ID(), 'mb_custom_url', true );
+					$item_url    = $custom_link ? $custom_link : get_the_permalink();
+					?>
+					<li>
+						<div class="Migrations__item__wrap">
+							<div class="Migrations__item__image">
+								<?php
+								if ( has_post_thumbnail() ) {
+									the_post_thumbnail( 'archive_thumbnail' );
+								} else {
+									?>
+									<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon-custom-post_type.svg"
+											 alt="<?php esc_attr_e( 'Features', 'ms' ); ?>">
+								<?php } ?>
+								<h3 class="Migrations__item__content__title item-title"><a
+										href="<?= esc_url( $item_url ); ?>"><?php the_title(); ?></a></h3>
+							</div>
+						</div>
+					</li>
+				<?php endwhile; ?>
+			</ul>
+		</div>
+	</div>
+</div>
