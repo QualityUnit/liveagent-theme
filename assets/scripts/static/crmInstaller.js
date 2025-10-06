5154d9e17c8bf9e5af1f898d506380c9aec37434
Ivan Hudak <36090254+ihdk@users.noreply.github.com>
2025-10-06 10:02:53 +0200

    fix: handle async trial installation, no progress needed (#3196)
    * fix: handle async trial installation, no progress needed
    
    * feat: async installation, continue browsing button
    
    * fix: small style fixes

--- assets/scripts/static/crmInstaller.js
+++ assets/scripts/static/crmInstaller.js
@@ -31,14 +31,7 @@ class CrmInstaller {
 	}
 
 	init = () => {
-		try {
-			const signupResponse = getCookie( 'trial_signup_response' );
-			this.signupData = JSON.parse( decodeURIComponent( signupResponse ) );
-		} catch ( error ) {
-			// eslint-disable-next-line no-console
-			console.error( error );
-		}
-
+		this.readSignUpCookie();
 		if ( ! this.signupData ) {
 			window.location.href = quCrmData.trialUrl;
 			return;
@@ -53,6 +46,12 @@ class CrmInstaller {
 		this.fields.introductionVideos = this.fields.main.querySelector( '.Introduction__videos' );
 		this.fields.progressDoneOverlay = this.fields.main.querySelector( '.progress__done__overlay' );
 		this.fields.redirectButtonPanel = this.fields.main.querySelector( '[data-id="redirectButtonPanel"]' );
+		this.fields.asyncInstallationButton = this.fields.main.querySelector( '.async-installation' );
+
+		if ( this.signupData.async === true ) {
+			this.handleAsyncInstallation();
+			return;
+		}
 
 		this.showHiddenParts();
 		this.handleInstallation();
@@ -97,9 +96,7 @@ class CrmInstaller {
 				this.setProgress( 100 );
 				this.handleFinishActions();
 				this.handleFinishOverlay();
-
-				// remove cookie after installation, installation process is available even the user refresh page or come back
-				setCookie( 'trial_signup_response', '', -1 );
+				this.removeSignUpCookie();
 
 				if ( data.login_url ) {
 					this.createGoToAppForm( data );
@@ -114,6 +111,13 @@ class CrmInstaller {
 		if ( ! response.success ) {
 			this.setProgressText( response.message );
 		}
+	};
+
+	handleAsyncInstallation = () => {
+		this.showAsyncInstallationButton();
+		this.setProgressText( this.localized.textProgressAsyncInstallation );
+		this.handleFinishActions();
+		this.removeSignUpCookie();
 	};
 
 	checkInstallationProgress = async () => {
@@ -252,6 +256,11 @@ class CrmInstaller {
 		} );
 	};
 
+	showAsyncInstallationButton = () => {
+		this.fields.asyncInstallationButton.style.display = 'block';
+		this.fields.progressPercentage.style.display = 'none';
+	};
+
 	runLoginExpirationCounter = ( goToButton ) => {
 		const counterElm = goToButton.querySelector( '.FinalButton__counter__number' );
 		const textElm = goToButton.querySelector( '.FinalButton__text' );
@@ -383,7 +392,24 @@ class CrmInstaller {
 
 	showHiddenParts = () => {
 		// by default, some parts like percentage and progress indicator are invisible in case the installation is processed for user with corrupted javascript
-		this.fields.progressHeader.querySelectorAll( '.invisible' ).forEach( ( elm ) => elm.classList.remove( 'invisible' ) );
+		this.fields.progressHeader.querySelectorAll( '.progress-invisible' ).forEach( ( elm ) => elm.classList.remove( 'progress-invisible' ) );
+	};
+
+	readSignUpCookie = () => {
+		try {
+			const cookieData = getCookie( 'trial_signup_response' );
+			if ( cookieData ) {
+				this.signupData = JSON.parse( decodeURIComponent( cookieData ) );
+			}
+		} catch ( error ) {
+			// eslint-disable-next-line no-console
+			console.error( error );
+		}
+	};
+
+	removeSignUpCookie = () => {
+		// remove cookie after installation, installation process is available even the user refresh page or come back
+		setCookie( 'trial_signup_response', '', -1 );
 	};
 
 	handlePapAction = () => {
--- template-thank-you.php
+++ template-thank-you.php
@@ -13,14 +13,16 @@ Trial_Signup::include_crm_installer();
 
 		<div class="BuildingApp">
 				<div class="BuildingApp__progress">
-					<div class="BuildingApp__progress__header">
-							<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon_logo_liveagent.jpg" alt="<?php bloginfo( 'name' ); ?>" class="BuildingApp__progress__header__logo">
-							<div class="BuildingApp__progress__header__title h2 loader-label"><?php _e( 'Building Your LiveAgent...', 'ms' ); ?></div>
-						<div class="BuildingApp__progress__header__action__wrapper invisible">
-							<div class="BuildingApp__progress__header__percentage percentage">0%</div>
+					<div class="BuildingApp__progress__header"> 
+						<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/icon_logo_liveagent.jpg" alt="<?php bloginfo( 'name' ); ?>" class="BuildingApp__progress__header__logo">
+						<div class="BuildingApp__progress__header__title h2 loader-label"><?php _e( 'Building Your LiveAgent...', 'ms' ); ?></div>
+							
+						<div class="BuildingApp__progress__header__action__wrapper">
+							<a class='FinalButton async-installation' href="<?= esc_url( home_url( '/' ) ); ?>" style="display: none;"><span class="FinalButton__text"><?= esc_html__( 'Continue browsing', 'ms' ); ?></span></a>
+							<div class="BuildingApp__progress__header__percentage percentage progress-invisible">0%</div>
 							<div data-id="redirectButtonPanel" style="display:none"></div>
-						</div>
-						<div id="progress-1" class="progress__bar__container invisible">
+						</div>						
+						<div id="progress-1" class="progress__bar__container progress-invisible">
 							<div class="progress__bar"></div>
 							<div class="progress__ball"></div>
 						</div>
